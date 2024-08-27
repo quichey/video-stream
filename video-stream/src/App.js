f5b070a052b468c6680347783405930221e40cb7
Quichey <kylengo357@gmail.com>
2024-08-27 11:01:38 -0700

    got it to work

--- video-stream/src/App.js
+++ video-stream/src/App.js
@@ -18,6 +18,17 @@ function App() {
           Learn React
         </a>
       </header>
+      <video controls width="2500" height="1000">
+        <source src="/media/cc0-videos/flower.webm" type="video/webm" />
+
+        <source src="https://chess-react.s3.us-west-1.amazonaws.com/WIN_20240826_10_03_57_Pro.mp4" type="video/mp4" />
+
+        Download the
+        <a href="/media/cc0-videos/flower.webm">WEBM</a>
+        or
+        <a href="/media/cc0-videos/flower.mp4">MP4</a>
+        video.
+      </video>
     </div>
   );
 }
