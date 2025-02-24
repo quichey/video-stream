fa2163c3e4d4632d013cdcfc110d77fc401fe16b
quichey <kylengo357@gmail.com>
2025-02-24 14:02:34 -0800

    added to Queue object for env variables -- fill in datatype conversions

--- backend/daemon/util/queue.py
+++ backend/daemon/util/queue.py
@@ -4,12 +4,25 @@ import util
 root_prefix = "QUEUE_"
 
 class Queue():
+    list_name = None
     
-    def __init__(product):
-        util.set_env(f"{root_prefix}_{product}","")
+    def __init__(self, product):
+        self.list_name = f"{root_prefix}_{product}"
+        util.set_env(self.list_name,"")
+
+    @property
+    def product_list(self):
+        _list = util.get_env(self.list_name)
+        _list = # however to convert string of comma delimited to python list
+        return _list
 
     def add(self, node):
-        pass
+        self.product_list.append(node)
+        env_list = # however convert python list to comma string
+        util.set_env(self.list_name, env_list)
 
     def get(self, node):
-        pass
+        for idx, curr_node in enumerate(self.product_list):
+            if curr_node == node:
+                return idx
+        raise Exception("item not found")
