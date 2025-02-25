5d8ff95efbe1bb46ad269abf4fe01a38d7994c21
root <root@DESKTOP-F4B1KO7>
2025-02-25 13:45:43 -0800

    start making Container class

--- backend/server/container/start.py
+++ backend/server/container/start.py
@@ -3,13 +3,35 @@ import os, shutil
 
 import server.util.env as curr_env
 
-# Create a socket object
-# AF_INET: IPv4 address family
-# SOCK_STREAM: TCP socket type
-curr_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+OS_SUPPORTED = ['linux']
 
-temp_pid = hash(curr_socket)
-curr_env.set_env("SERVER_PID", temp_pid)
+class Container():
+    sockets = []
+
+    def __init__(self, os = 'linux'):
+        if os not in OS_SUPPORTED:
+            raise Exception("OS not supported")
+
+    # Create a socket object
+    # AF_INET: IPv4 address family
+    # SOCK_STREAM: TCP socket type
+    def ready_socket():
+        curr_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        temp_pid = hash(curr_socket)
+        # probably don't need to save this stuff to the bash if the python shell is able to stay up
+        # check if python shell can still compute while windows os is in sleep mode
+        # possibly can with wsl
+        curr_env.set_env("SERVER_PID", temp_pid)
+
+        sockets.append(curr_socket)
+    
+    def remove_socket(socket_hash):
+        self.sockets = [s for s in self.sockets if hash(s) is not socket_hash]
+
+        # remove from bash if bash env variables are necessary
+        
+
 
 
 # need to bind the server socket to accept connections from clients
