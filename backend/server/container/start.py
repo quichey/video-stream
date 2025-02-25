5370176f714cec46a5e2df42eee27ccca9b3258c
root <root@DESKTOP-F4B1KO7>
2025-02-25 13:10:55 -0800

    practice using socket library and plan out blueprint of server container that can serve multiple client connections

--- backend/server/container/start.py
+++ backend/server/container/start.py
@@ -10,3 +10,9 @@ curr_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 temp_pid = hash(curr_socket)
 curr_env.set_env("SERVER_PID", temp_pid)
+
+
+# need to bind the server socket to accept connections from clients
+# create a queue of server sockets to be able to facilitate multiple clients
+# use Queue class in util
+# instantiate with socket hashes as names and values be set to the port that it is bound to
