8ef57e3268ba5be5605c9224c797186336301943
root <root@DESKTOP-F4B1KO7>
2025-03-04 16:29:22 -0800

    rename frontend to client and backend to server for clearer encapsulation

---
+++ server/server/util/Internet/protocol.py
@@ -0,0 +1,11 @@
+
+class PROTOCOL(CHORD):
+    def __init__(self):
+        pass
+
+class HTTP(PROTOCOL):
+    def __init__(self):
+        pass
+    
+    def send_message(self, giver, receiver, message_type):
+        pass
