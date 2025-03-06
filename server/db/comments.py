249d5511b182791db95cb2988bf25ec4dd500fe0
quichey <kylengo357@gmail.com>
2025-03-06 14:05:20 -0800

    use python to define tables -- look into using sqlalchemy for data seeding

---
+++ server/db/comments.py
@@ -0,0 +1,6 @@
+import Table from Table
+
+class Comments(Table):
+    comment = str
+    user_id = int
+    pass
--- server/db/load_db.py
+++ server/db/load_db.py
@@ -3,3 +3,13 @@ def create_table(table_name: str, columns: list):
     # second depth is 2-tuple of (column_name, data_type)
     pass
 
+def create_random_value(data_type):
+    # do case switch on data_type
+    pass
+
+def create_random_record(table):
+    # get table specs for table object
+    # get column names and column types
+    # use create_random_value
+    # sqlalchemy may have function available to do this
+    pass
