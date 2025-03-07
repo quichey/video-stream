181885882c3ad5aa9f990f29938ac75c81c320dd
root <root@DESKTOP-F4B1KO7>
2025-03-07 12:50:04 -0800

    practice using sqlalchemy create_engine

---
+++ server/db/sqlalchemy_practice.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+
+# create engine
+def start_engine():
+    dialect = "mysql"
+    db_api = "mysqlconnector"
+
+    user = "new_user"
+    pw = "password"
+    hostname = "localhost:3306"
+    dbname = "video_stream"
+    url = f"{user}:{pw}@{hostname}/{dbname}"
+
+    engine = create_engine(f"{dialect}+{db_api}://{url}", echo=True)
