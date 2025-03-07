41a7bc733711a3581873acffd56b0851b71ea70e
quichey <kylengo357@gmail.com>
2025-03-07 14:38:29 -0800

    add blueprint file for defining the DB structure/definitions

--- server/db/sqlalchemy_practice.py
+++ server/db/sqlalchemy_practice.py
@@ -2,26 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy import text
 
 
-class DB_CONNECTOR():
-
-    def __init__(self):
-        pass
-    
-    def create_database(self):
-        pass
-
-    def create_table(self):
-        pass
-
-    def populate_table(self, table):
-        pass
-
-    def seed_db(self):
-        # create_database
-        # create_table(s)
-        # populate_tables
-        # maybe save a backup of database as archive copy of state
-        pass
 
 # create engine
 def start_engine():
