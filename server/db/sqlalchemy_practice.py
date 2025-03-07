8b8133e5a0465ea41da4fcba6a9c77eaabc49eb9
root <root@DESKTOP-F4B1KO7>
2025-03-07 14:05:35 -0800

    practice engine connection and blueprint the DB_CONNECTOR class for seeding db as well probably for APIs in future branches

--- server/db/sqlalchemy_practice.py
+++ server/db/sqlalchemy_practice.py
@@ -1,4 +1,27 @@
 from sqlalchemy import create_engine
+from sqlalchemy import text
+
+
+class DB_CONNECTOR():
+
+    def __init__(self):
+        pass
+    
+    def create_database(self):
+        pass
+
+    def create_table(self):
+        pass
+
+    def populate_table(self, table):
+        pass
+
+    def seed_db(self):
+        # create_database
+        # create_table(s)
+        # populate_tables
+        # maybe save a backup of database as archive copy of state
+        pass
 
 # create engine
 def start_engine():
@@ -12,3 +35,10 @@ def start_engine():
     url = f"{user}:{pw}@{hostname}/{dbname}"
 
     engine = create_engine(f"{dialect}+{db_api}://{url}", echo=True)
+    return engine
+
+
+def connect_engine(engine):
+    with engine.connect() as conn:
+        result = conn.execute(text("select 'hello world'"))
+        print(result.all())
