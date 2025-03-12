56f1700331b579080dcb8748c21036c451aa28c7
root <root@DESKTOP-F4B1KO7>
2025-03-11 18:08:52 -0700

    add comment blue-printing out snapshotting the full database for optimality/inter-operability b/t db drivers

--- server/db/load_db.py
+++ server/db/load_db.py
@@ -1,3 +1,12 @@
+# may expand this file to be named snapshot_db
+# to draw inspiration from ISS/Clinicomp as well as the
+# glorious game Pokemon Snap
+# -- ideally i would make functionality to
+# - Check if database named video_stream exists
+# - - if not, create it with a test user
+# - create/load tables with demo-able data
+# - prepare indices for optimal searching/fetching for appropriate cases
+
 def create_table(table_name: str, columns: list):
     # columns is nested list depth 2
     # second depth is 2-tuple of (column_name, data_type)
