7baa1bd155bf446ca4ca7ad349b15d570b45c4f4
quichey <kylengo357@gmail.com>
2025-03-13 13:21:41 -0700

    update schema to facilitate comment threads, likes/dislikes, and edited comments

--- server/db/DB_SCHEMA.py
+++ server/db/DB_SCHEMA.py
@@ -1,5 +1,5 @@
 from sqlalchemy import MetaData
-from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy import Table, Column, Boolean, Integer, String
 from sqlalchemy import ForeignKey
 
 metadata_obj = MetaData()
@@ -18,12 +18,29 @@ users_table = Table(
     "users",
     metadata_obj,
     Column("id", Integer, primary_key=True),
+
     Column("name", String(30)),
+    Column("email", String(30)),
 )
 
 comments_table = Table(
     "comments",
     metadata_obj,
+    Column("id", Integer, primary_key=True),
     Column("user_id", ForeignKey("users.id"), nullable=False),
+    Column("thread_comment_id", ForeignKey("comments.id"), nullable=True),
+
     Column("comment", String(100)),
+    Column("date", String(30)),
+    Column("edited_flag", Boolean),
+)
+
+comment_likes_table = Table(
+    "comment_likes",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("comment_id", ForeignKey("comments.id"), nullable=False),
+    Column("user_id", ForeignKey("users.id"), nullable=False),
+
+    Column("like_dislike_flag", Boolean),
 )
