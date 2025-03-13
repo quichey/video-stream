fecc5f3fe64df3ab045c17c0bc02f8c1752a0899
root <root@DESKTOP-F4B1KO7>
2025-03-13 14:48:10 -0700

    add Seed.py as a utility program to simplify starting up a database -- optionally with test data -- from a given schema

--- server/db/load_db.py
+++ server/db/load_db.py
@@ -1,24 +1,9 @@
-# may expand this file to be named snapshot_db
-# to draw inspiration from ISS/Clinicomp as well as the
-# glorious game Pokemon Snap
-# -- ideally i would make functionality to
-# - Check if database named video_stream exists
-# - - if not, create it with a test user
-# - create/load tables with demo-able data
-# - prepare indices for optimal searching/fetching for appropriate cases
+import db.DB_SCHEMA as schema
+import db.Seed as seed
 
-def create_table(table_name: str, columns: list):
-    # columns is nested list depth 2
-    # second depth is 2-tuple of (column_name, data_type)
-    pass
+# This is the unifying script that will use DB_SCHEMA.py in conjunction with Seed.py
+# To initiate the state of this specific video-stream web-app
 
-def create_random_value(data_type):
-    # do case switch on data_type
-    pass
-
-def create_random_record(table):
-    # get table specs for table object
-    # get column names and column types
-    # use create_random_value
-    # sqlalchemy may have function available to do this
-    pass
+# Think about how I intend to use this script
+# -- the different programs/options to run this script
+# -- and then plan out the functions/fields/etc
