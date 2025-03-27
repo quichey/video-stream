17b8c0345d263bb1d8be90c7f2987ca4a8273df0
root <root@DESKTOP-F4B1KO7>
2025-03-27 14:03:58 -0700

    start the api_gateway.py program to serve as the running process for the server-linkage pathway

--- server/api/routes.py
+++ server/api/routes.py
@@ -31,135 +31,136 @@ class DB():
         return self.conn.cursor()
 
 
-# TODO: add initial route for establishing websocket line of communication
-# guessing just add route("/ws") and return status 200 for OK
-# probably should check flask or http library for proper headers etc
-@app.route('/ws')
-def add_client():
-    global clients
-    clients.append("test")
-    return 'STATUS 200 OK'
+class Routes():
+    # TODO: add initial route for establishing websocket line of communication
+    # guessing just add route("/ws") and return status 200 for OK
+    # probably should check flask or http library for proper headers etc
+    @app.route('/ws')
+    def add_client():
+        global clients
+        clients.append("test")
+        return 'STATUS 200 OK'
 
 
-# Route to get all comments
-@app.route('/blah')
-def html_comments():
-    db = DB()
-    conn = db.cursor()
-    query = """
-        SELECT
-            c.comment,
-            u.name as user_name
-        FROM
-        comments AS c
-        LEFT JOIN
-        users AS u
-        ON c.user_id = u.id;
-    """
-    conn.execute(query)
-    items = conn.fetchall()
-    conn.close()
-    html = "<u1>"
-    for record in items:
-        comment = record[0]
-        user_name = record[1]
-        html += f"<li>@{user_name}: {comment}</li>"
-    html += "<ui>"
-    return html
+    # Route to get all comments
+    @app.route('/blah')
+    def html_comments():
+        db = DB()
+        conn = db.cursor()
+        query = """
+            SELECT
+                c.comment,
+                u.name as user_name
+            FROM
+            comments AS c
+            LEFT JOIN
+            users AS u
+            ON c.user_id = u.id;
+        """
+        conn.execute(query)
+        items = conn.fetchall()
+        conn.close()
+        html = "<u1>"
+        for record in items:
+            comment = record[0]
+            user_name = record[1]
+            html += f"<li>@{user_name}: {comment}</li>"
+        html += "<ui>"
+        return html
 
 
-# Route to get all items
-@app.route('/comments', methods=["GET"])
-def read_comments_temp():
-    db = DB()
-    conn = db.cursor()
-    query = """
-        SELECT
-            c.comment,
-            u.name as user_name
-        FROM
-        comments AS c
-        LEFT JOIN
-        users AS u
-        ON c.user_id = u.id;
-    """
-    conn.execute(query)
-    items = conn.fetchall()
-    conn.close()
-    data = []
-    for record in items:
-        comment = record[0]
-        user_name = record[1]
-        data.append({"user_name": user_name, "comment": comment})
-    data = jsonify(data)
-    return data
-
-# Route to get all items using infinite scroll
-"""
-can use different request attributes to make the route url of 
-this infinite scroll paging not weird
-
-something like request.form['infinite_scroll'] = True
-and then do the listing code
-and otherwise update the DB table or something
-
-
-so after thinking, first request should return a larger amount of comments
-than later requests that emulate infinte-scrolling, so that the UI is fluid
-"""
-@app.route('/getcomments', methods=["POST"])
-def read_comments():
-    # the above is an example of getting data from the POST request
-
-    # temp instantiation of Cache object
-    cache = Cache()
-
-    # TODO: change later to something like request.form['username']
-    user_info = "random_stuff_for_now"
-    try:
-        session_info = cache.get_session(user_info)
-    except SecurityError as security_alarm:
-        data = {
-            "status": "error",
-            "msg": security_alarm.msg()
-        }
-        # later, check Flask docs
-        # update with flask framework
+    # Route to get all items
+    @app.route('/comments', methods=["GET"])
+    def read_comments_temp():
+        db = DB()
+        conn = db.cursor()
+        query = """
+            SELECT
+                c.comment,
+                u.name as user_name
+            FROM
+            comments AS c
+            LEFT JOIN
+            users AS u
+            ON c.user_id = u.id;
+        """
+        conn.execute(query)
+        items = conn.fetchall()
+        conn.close()
+        data = []
+        for record in items:
+            comment = record[0]
+            user_name = record[1]
+            data.append({"user_name": user_name, "comment": comment})
+        data = jsonify(data)
         return data
-    """
-    Handle different cases of different combinations
-    of User_id with session_id
-    null case: session_id is empty
-    then: generate new session --- do i need extra code for this? i don't think so
-    case 1: user_id does not match value of session_id
-    then: return error code as most likely spoofing attempt
-    """
 
-    comment_data = cache.get_comments(session_info)
-    data = {
-        "session_info": session_info,
-        "comment_data": comment_data,
-    }
-    data = jsonify(data)
-    return data
+    # Route to get all items using infinite scroll
+    """
+    can use different request attributes to make the route url of 
+    this infinite scroll paging not weird
+
+    something like request.form['infinite_scroll'] = True
+    and then do the listing code
+    and otherwise update the DB table or something
 
 
-# Route to create a new item
-@app.route('/comments', methods=['POST'])
-def create_item():
-    item_data = request.get_json()
-    db = DB()
-    cursor = db.cursor()
-    # TODO: update query
-    query = """
-        INSERT INTO comments (comment, user_id, date)
-        VALUES (?,?);
+    so after thinking, first request should return a larger amount of comments
+    than later requests that emulate infinte-scrolling, so that the UI is fluid
     """
-    cursor.execute(query,
-                   (item_data['name'], item_data['description']))
-    db.commit()
-    db.close()
-    return jsonify({'message': 'Comment created successfully'}), 201
+    @app.route('/getcomments', methods=["POST"])
+    def read_comments():
+        # the above is an example of getting data from the POST request
+
+        # temp instantiation of Cache object
+        cache = Cache()
+
+        # TODO: change later to something like request.form['username']
+        user_info = "random_stuff_for_now"
+        try:
+            session_info = cache.get_session(user_info)
+        except SecurityError as security_alarm:
+            data = {
+                "status": "error",
+                "msg": security_alarm.msg()
+            }
+            # later, check Flask docs
+            # update with flask framework
+            return data
+        """
+        Handle different cases of different combinations
+        of User_id with session_id
+        null case: session_id is empty
+        then: generate new session --- do i need extra code for this? i don't think so
+        case 1: user_id does not match value of session_id
+        then: return error code as most likely spoofing attempt
+        """
+
+        comment_data = cache.get_comments(session_info)
+        data = {
+            "session_info": session_info,
+            "comment_data": comment_data,
+        }
+        data = jsonify(data)
+        return data
+
+
+    # Route to create a new item
+    @app.route('/comments', methods=['POST'])
+    def create_item():
+        item_data = request.get_json()
+        db = DB()
+        cursor = db.cursor()
+        # TODO: update query
+        query = """
+            INSERT INTO comments (comment, user_id, date)
+            VALUES (?,?);
+        """
+        cursor.execute(query,
+                    (item_data['name'], item_data['description']))
+        db.commit()
+        db.close()
+        return jsonify({'message': 'Comment created successfully'}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
--- server/api/test.py
+++ server/api/test.py
@@ -1,7 +1,7 @@
 from flask import request
 
-with app.test_request_context('/hello', method='POST'):
+with app.test_request_context('/getcomments', method='POST'):
     # now you can do something with the request until the
     # end of the with block, such as basic assertions:
-    assert request.path == '/hello'
+    assert request.path == '/getcomments'
     assert request.method == 'POST'
