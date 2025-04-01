74a45dcb0157c0a5436093510229c42de1c01684
root <root@DESKTOP-F4B1KO7>
2025-04-01 12:16:05 -0700

    practice -- created some Mapped classes with the Declarative Base

--- server/db/sqlalchemy_practice.py
+++ server/db/sqlalchemy_practice.py
@@ -149,3 +149,34 @@ def print_base_metadata():
 
 def print_base_registry():
     print(f"Base.registry: {Base.registry}")
+
+
+
+from typing import List
+from typing import Optional
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+class User(Base):
+    __tablename__ = "user_account"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+    addresses: Mapped[List["Address"]] = relationship(back_populates="user")
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+class Address(Base):
+    __tablename__ = "address"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str]
+    user_id = mapped_column(ForeignKey("user_account.id"))
+    user: Mapped[User] = relationship(back_populates="addresses")
+    def __repr__(self) -> str:
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+
+
+def create_user():
+    sandy = User(name="sandy", fullname="Sandy Cheeks")
+    return sandy
