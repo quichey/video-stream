eb11be99f16ecc9d09c7f4341df2b3578fb34ac8
quichey <kylengo357@gmail.com>
2025-04-02 10:31:08 -0700

    start fixture for setting up test_user fixture -- fill in with missing logic

--- server/api/tests/comments.py
+++ server/api/tests/comments.py
@@ -1,4 +1,24 @@
+import pytest
 from flask import request
+
+from api import create_app
+
+@pytest.fixture()
+def initiate_test_user():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # get info of test_user
+    # include info in yield
+
+    yield app
+
+    # clean up / reset resources here
+
+
+
 
 with app.test_request_context('/getcomments', method='POST'):
     # now you can do something with the request until the
