a68d7fae4f4e7b186724527548ebc8179561a593
root <root@DESKTOP-F4B1KO7>
2025-04-02 10:49:42 -0700

    added get_first_page func and blueprint next steps

--- server/api/tests/comments.py
+++ server/api/tests/comments.py
@@ -17,9 +17,34 @@ def initiate_test_user():
 
     # clean up / reset resources here
 
+def get_first_page(client):
+    response = client.post(
+        "/getcomments",
+        options={
+            "data": {
+                "user_id": 0,
+                "user_name": "test_user"
+            }
+        }
+    )
+    num_comments = len(response.data)
+    assert num_comments > 0
+    assert num_comments < 1000
+    # assert the session token is either
+    # returned here or the login
+    # http request
+
+    # when I add into Seed module
+    # the code for instantiating 
+    # a consistent testing database
+    # ensure the num_comments == size_of_first_page
 
 
-
+"""
+May use this context manager
+as a quick means of running through all tests
+in the order in which they should be applied (imperative programming paradigm)
+"""
 with app.test_request_context('/getcomments', method='POST'):
     # now you can do something with the request until the
     # end of the with block, such as basic assertions:
