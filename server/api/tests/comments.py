964185e214bd5b59583c7ba3966f7588e3e6534b
quichey <kylengo357@gmail.com>
2025-04-08 15:30:14 -0700

    tried various things to get threading working -- i think should just forget about it and put time.sleep(a long time) to move on with the testing

--- server/api/tests/comments.py
+++ server/api/tests/comments.py
@@ -98,7 +98,6 @@ def get_next_page(client, token, user):
     return results
 
 
-
 """
 May use this context manager
 as a quick means of running through all tests
@@ -150,10 +149,99 @@ def test_infinite_scroll(app_info):
         prev_thread.join(thread)
         return thread
 
-    prev_thread = create_first_thread()
 
+    def send_next_http_request():
+        time.sleep(1000)
+        nonlocal token
+        results = get_next_page(client, token["value"], test_user)
+        time_delta = results["time_delta"]
+        assert time_delta < next_page_time_standard #TODO: determine appropriate threshold
+        assert results["token"] == token
+        return
+
+    first_thread = create_first_thread()
+    threads = [first_thread]
     num_pages_to_test = 10
     for i in range(num_pages_to_test):
+        print(f"\n\n num next pages: {i} \n\n")
+        prev_thread = threads[-1]
+        thread = threading.Thread(target=send_next_http_request)
+        thread.start()
+        prev_thread.run()
+        #prev_thread.join(thread)
+        thread.join(prev_thread)
+        threads.append(thread)
+        # TODO: i think set next_thread = create_next_thread and curr_thread = next_thread
+        # first,,, read more docs
+        """
+        # TODO: assert latencies of each request
+        results = get_next_page(client, token, test_user)
+        time_delta = results["time_delta"]
+        assert time_delta < next_page_time_standard #TODO: determine appropriate threshold
+        assert results["token"] == token
+        # Possible scalability/security measure
+        # switch token every request
+        # WAAAY down the line
+        print(f"\n\n num next pages: {i} \n\n")
+        """
+
+
+
+"""
+May use this context manager
+as a quick means of running through all tests
+in the order in which they should be applied (imperative programming paradigm)
+
+curl --header "Content-Type: application/json" --request POST --data '{"user_id":"0","user_name":"users_name_0"}' http://127.0.0.1:5000/getcomments
+curl --header "Content-Type: application/json" --request POST --data '{"user_id":"0","user_name":"users_name_0", "token": "0"}' http://127.0.0.1:5000/getcomments
+"""
+def test_infinite_scroll_2(app_info):
+    next_page_time_standard = 100
+
+    client = app_info["client"]
+    test_user = app_info["test_user"]
+    #token = get_first_page(client, test_user)
+    token = {
+        "value": None
+    }
+    def create_first_thread():
+        nonlocal token
+        def send_http_request():
+            nonlocal token
+            token_value = get_first_page(client, test_user)
+            token["value"] = token_value
+            return
+        thread = threading.Thread(
+            target=send_http_request
+        )
+        thread.start()
+        return thread
+
+    def create_next_thread(prev_thread):
+        # TODO: read more about threading, then think CAREFULLY
+        # then do the things you think are good to do
+
+        nonlocal token
+        def send_http_request():
+            time.sleep(1000)
+            nonlocal token
+            results = get_next_page(client, token["value"], test_user)
+            time_delta = results["time_delta"]
+            assert time_delta < next_page_time_standard #TODO: determine appropriate threshold
+            assert results["token"] == token
+            return
+
+
+        thread = threading.Thread(target=send_http_request)
+        thread.start()
+        prev_thread.run()
+        prev_thread.join(thread)
+        return thread
+
+    prev_thread = create_first_thread()
+    num_pages_to_test = 10
+    for i in range(num_pages_to_test):
+        print(f"\n\n num next pages: {i} \n\n")
         next_thread = create_next_thread(prev_thread)
         prev_thread = next_thread
         # TODO: i think set next_thread = create_next_thread and curr_thread = next_thread
