35ce6fecd471f487e43939c4d2baee93e765bdd9
root <root@DESKTOP-F4B1KO7>
2025-05-05 21:37:51 -0700

    re-learned that sqlalchemy create_database_definition does not create the database itself, it only creates the tables w/in existing database -- added extra code to create the backup database

--- server/db/Seed/Seed.py
+++ server/db/Seed/Seed.py
@@ -9,7 +9,7 @@ from typing import Optional
 import sqlalchemy as sql
 from sqlalchemy import create_engine
 from sqlalchemy import Boolean, Integer, String, DateTime
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, sessionmaker
 
 
 #Sketching out blueprint/concepts of Seed class vs Cache class
@@ -79,12 +79,13 @@ class Seed():
 
     def __init__(self, admin_specs, database_specs, schema):
         self.admin_specs = DataBaseSpec(**admin_specs)
-        self.database_specs = database_specs
+        self.database_specs = DataBaseSpec(**database_specs)
         self.schema = schema
         self.base = schema.Base
         self.metadata_obj = schema.Base.metadata
         self.construct_engine()
         self.construct_admin_engine()
+        self.back_up_counter = 0
 
     def get_table_metadata(self, table_name):
         return self.metadata_obj.tables[table_name]
@@ -110,15 +111,17 @@ class Seed():
         hostname = database_specs.hostname
         dbname = database_specs.dbname 
         url = f"{dialect}+{db_api}://{user}:{pw}@{hostname}"
-        if dbname is not "":
+        if dbname != "":
             url += f"/{dbname}"
         return url
 
+    @property
+    def back_up_db_name(self):
+        return f"{self.database_specs.dbname}_{self.back_up_counter}"
 
     def construct_back_up_engine(self):
         db_specs = self.admin_specs
-        counter = 2
-        db_specs.dbname = f"{self.database_specs.dbname}_{counter}"
+        db_specs.dbname = self.back_up_db_name
         conn_str = self.construct_db_conn_str(db_specs)
 
         engine = create_engine(conn_str, echo=True)
@@ -144,7 +147,19 @@ class Seed():
     def create_database_definition(self, engine=None):
         if engine is None:
             engine = self.engine
+        print(f"\n\n creating ddl: {engine} \n\n")
         self.metadata_obj.create_all(engine)
+
+        
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        # Optionally, commit the changes to the database
+        session.commit()
+
+        # Close the session
+        session.close()
+
         return
     
     def is_a_primary_key(self, table, key):
@@ -190,6 +205,14 @@ class Seed():
         # like i did at ISS
         pass
 
+    def create_database(self, admin_session, db_name):
+        dialect = self.admin_specs.dialect
+        query = ""
+        match dialect:
+            case "mysql":
+                query = f"CREATE DATABASE {db_name}"
+        admin_session.execute(sql.text(query))
+        return
     """
     TODO: ?
     may need or want to get rid of database_specs to
@@ -197,8 +220,11 @@ class Seed():
     or finally understand python multithreading library
     """
     def back_up_db(self, admin_session):
+        back_up_db_name = self.back_up_db_name
+        self.create_database(admin_session, back_up_db_name)
         back_up_engine = self.construct_back_up_engine()
         self.create_database_definition(back_up_engine)
+
         #TODO:
         # - check for concurrency problems
         dialect = self.admin_specs.dialect
@@ -287,7 +313,16 @@ class Seed():
 
     def init_ddl(self):
         with Session(self.admin_engine) as admin_session:
-            exists = True #TODO: check if exists
+            db_name = self.database_specs.dbname
+            query = "SHOW DATABASES"
+            db_records = admin_session.execute(sql.text(query))
+            exists = False
+            for r in db_records:
+                if r[0] == db_name:
+                    exists = True
+                elif db_name in r[0]:
+                    self.back_up_counter = self.back_up_counter + 1
+
             if exists:
                 self.back_up_db(admin_session)
             self.create_database_definition()
