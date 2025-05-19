e7466789f5296a09f202c6f9b34b77f302608434
root <root@DESKTOP-F4B1KO7>
2025-05-19 12:49:10 -0700

    fill in blueprint of VideoFileManager

--- server/db/Schema/Video/VideoFile.py
+++ server/db/Schema/Video/VideoFile.py
@@ -1,9 +1,26 @@
+from dataclasses import dataclass
+
+@dataclass
 class VideoFile():
+	file_name = str
+	file_location = str
+	video_id = int
+
+
+class VideoFileManager():
 	def __init__(self):
+		self.videos = {}
+		pass
+	
+
+	def create_video(self, file_name, file_location):
 		pass
 
-	def save_file(file_name):
+	def read_video(self, video_id):
 		pass
 
-	def get_file(file_name):
+	def update_video(self, video_id, file_location, file_name=None):
+		pass
+
+	def delete_video(self, video_id):
 		pass
