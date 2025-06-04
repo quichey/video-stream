1d2d9cb1c823953b824b914d82fcee604e0ca1d2
root <root@DESKTOP-F4B1KO7.localdomain>
2025-06-03 17:25:30 -0700

    got the api to not error out but client is sending an extraneous request

--- server/api/Routers/Router.py
+++ server/api/Routers/Router.py
@@ -1,9 +1,17 @@
 from flask import json
+from dataclasses import dataclass
 
 """
 Thinking maybe have this class to handle any common Flask
 Logic that I see in ClientRouter and AdminRouter
 """
+
+@dataclass
+class VideoUpload():
+    name: str
+    bytes: bytes
+    user_id: int
+    upload_date: str
 
 class Router():
     def __init__(self, app, cache, request):
@@ -43,12 +51,24 @@ class Router():
         if "file" in form_data:
             file = form_data['file']
             print(f"file: {file}")
-            video_file_info["stream"] = file
-        video_file_info["stream"] = self.decode_video(video_file_info["stream"])
+            # file being loaded as str
+            # convert to something consumable by bytes() construct
+            video_file_info["bytes"] = dict(file)
+            video_file_info["name"] = "tmp"
+            video_file_info["user_id"] = 0
+            video_file_info["upload_date"] = "now"
+        video_file_info["bytes"] = self.decode_video(video_file_info["bytes"])
+        video_file_info = VideoUpload(**video_file_info)
         return video_file_info
 
     def decode_video(self, file_stream):
-        as_bytes = bytes(file_stream)
+        as_array = []
+        for _, b in file_stream.items():
+            #print(f"\n\n bytes(b): {bytes(b)} \n\n")
+            as_array.append(b)
+        print(f"\n\n as_array: {as_array} \n\n")
+        as_bytes = bytes(as_array)
+        print(f"\n\n as_bytes: {as_bytes} \n\n")
         return as_bytes.decode('utf-8', errors='ignore')
 
     def set_up(self):
