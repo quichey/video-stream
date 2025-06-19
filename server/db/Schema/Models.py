be5b0daf075f711ae3c001ed75a7511448c75660
root <root@DESKTOP-F4B1KO7.localdomain>
2025-06-19 12:48:53 -0700

    fixed up layout

--- server/db/Schema/Models.py
+++ server/db/Schema/Models.py
@@ -89,6 +89,7 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
     email: Mapped[Optional[str]] = mapped_column(String(30))
+    profile_icon: Mapped[Optional[str]] = mapped_column(String(30))
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, name={self.name!r}, email={self.email!r})"
