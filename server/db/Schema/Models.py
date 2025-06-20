e3d12b5e7d12d5244dee8171429281925ab9e715
quichey <kylengo357@gmail.com>
2025-06-20 16:59:26 -0700

    add date timestamps to Video Model

--- server/db/Schema/Models.py
+++ server/db/Schema/Models.py
@@ -124,8 +124,8 @@ class Video(Base):
     file_name: Mapped[str] = mapped_column(String(30))
     file_dir: Mapped[str] = mapped_column(String(100))
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
-
-
+    date_updated: Mapped[str] = mapped_column(DateTime)
+    date_created: Mapped[str] = mapped_column(DateTime)
 
     def __repr__(self) -> str:
         return f"Video(id={self.id!r}, file={self.file!r}, user_id={self.user_id!r})"
