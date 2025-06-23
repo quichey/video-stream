84354d3811de63fe1cfc62d857d5ea6eb047a12c
quichey <kylengo357@gmail.com>
2025-06-23 12:19:21 -0700

    format date as time ago

--- client/src/pages/home/VideoTile/VideoInfo.js
+++ client/src/pages/home/VideoTile/VideoInfo.js
@@ -8,6 +8,43 @@ import { VideoContext } from "../..";
 
 import UserIcon from "../../user/UserIcon";
 
+function formatTimeAgo(date) {
+  const now = new Date();
+  const seconds = Math.floor((now - date) / 1000);
+
+  if (seconds < 60) {
+    return new Intl.RelativeTimeFormat('en').format(-seconds, 'second');
+  }
+
+  const minutes = Math.floor(seconds / 60);
+  if (minutes < 60) {
+    return new Intl.RelativeTimeFormat('en').format(-minutes, 'minute');
+  }
+
+  const hours = Math.floor(minutes / 60);
+  if (hours < 24) {
+    return new Intl.RelativeTimeFormat('en').format(-hours, 'hour');
+  }
+
+  const days = Math.floor(hours / 24);
+  if (days < 7) {
+    return new Intl.RelativeTimeFormat('en').format(-days, 'day');
+  }
+
+  const weeks = Math.floor(days / 7);
+  if (weeks < 4) { // Approximately a month
+    return new Intl.RelativeTimeFormat('en').format(-weeks, 'week');
+  }
+
+  const months = Math.floor(days / 30.44); // Average days in a month
+  if (months < 12) {
+    return new Intl.RelativeTimeFormat('en').format(-months, 'month');
+  }
+
+  const years = Math.floor(days / 365.25); // Average days in a year
+  return new Intl.RelativeTimeFormat('en').format(-years, 'year');
+}
+
 export default function VideoInfo({ title, userName, userIcon, totalViews, uploadDate, userID, videoID }) {
   const { setID, setName } = React.useContext(ChannelContext);
   const { setID: setVideoID } = React.useContext(VideoContext);
@@ -45,7 +82,7 @@ export default function VideoInfo({ title, userName, userIcon, totalViews, uploa
             </NavLink>
             <Stack direction="row" spacing={1}>
                 <Typography>{totalViews}</Typography>
-                <Typography>{uploadDate}</Typography>
+                <Typography>{formatTimeAgo(new Date(uploadDate))}</Typography>
             </Stack>
         </Stack>
     </Stack>
