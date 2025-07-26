4dedb8c96f02f10595bf94b9dac6c32043054f66
root <root@DESKTOP-F4B1KO7.localdomain>
2025-07-25 18:07:21 -0700

    add util bash for detecting machine (local vs cloud)

--- cloud/deploy/util/util.sh
+++ cloud/deploy/util/util.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
 
 ###########
 #
@@ -12,3 +15,26 @@ get_curr_subdir() {
 }
 
 get_curr_subdir
+
+
+###########
+#
+# DETECT MACHINE
+#
+###########
+: "${DEPLOY_ENV:=local}"  # default to local if not set
+
+case "$DEPLOY_ENV" in
+  cloud)
+    echo "Cloud setup"
+    SERVER_HOST="cloud-db.example.com"
+    ;;
+  local)
+    echo "Local setup"
+    SERVER_HOST="localhost"
+    ;;
+  *)
+    echo "Unknown DEPLOY_ENV: $DEPLOY_ENV"
+    exit 1
+    ;;
+esac
