1d2f74eb255b911ffd1b8817b5f8000d59c41bb8
root <root@DESKTOP-F4B1KO7.localdomain>
2025-07-31 16:43:38 -0700

    chatgpt assisted revisions

--- cloud/deploy/client/setup.sh
+++ cloud/deploy/client/setup.sh
@@ -1,56 +1,42 @@
 #!/usr/bin/env bash
 
-# sets bash script environment to be robust w/error-handling
+# robust error-handling
 set -eEo pipefail
 
-###########
-#
-# getting location of where this script is run from
-#
-###########
+# load utilities and env vars
 source util/util.sh
 source util/env_vars.sh client
 
-# location of where this script is run needs to be tracked
 curr_dir=$relative_subdir
-location_of_this_script_called='cloud/deploy' # adjust if different
+location_of_this_script_called='cloud/deploy'
 location_of_client_subdir='../../client'
-
-########
-#
-# Automate loading cloud/Docker/...Dockerfiles into 
-# correct subdirs such as from cloud/Docker/client/client.Dockerfile into client/Dockerfile
-#
-########
-
-##
-## Hoping I can have just one script to run from here
-##
-
 
 printf "Installing React app dependencies...\n"
 cd $location_of_client_subdir
 npm install
 printf "Completed.\n\n"
 
-printf "Building React app and placing into client/public/. ...\n"
+# Build frontend HTML (into public/)
+printf "Building React frontend and placing into client/public/ ...\n"
 npm run build
+printf "Completed.\n\n"
+
+# Build daemon code (into daemon/)
+printf "Preparing daemon code into client/daemon/ ...\n"
+npm run build:daemon
 printf "Completed.\n\n"
 
 printf "Setup completed successfully!\n"
 
-# back to deploy folder
+# Back to deploy folder
 cd ../${location_of_this_script_called}
 
 case "$DEPLOY_ENV" in
   cloud)
-    # set Version
     : "${VERSION:=1}"  # default to 1 if not set
 
-    # Copy Dockerfile from cloud to client folder
     cp ../Docker/client/client.Dockerfile ../../client/Dockerfile
 
-    # Submit cloud build
     gcloud builds submit $location_of_client_subdir \
       --tag gcr.io/${GOOGLE_CLOUD_PROJECT}/client-dev-test:${VERSION}.0.0
     ;;
@@ -62,4 +48,3 @@ case "$DEPLOY_ENV" in
     exit 1
     ;;
 esac
-
