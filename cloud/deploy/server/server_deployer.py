00155cd9e96f6e45abb989b3111c67fdf2433fcd
root <root@DESKTOP-F4B1KO7.localdomain>
2025-08-03 13:47:04 -0700

    fix python modules based off of inability to change parent shell's env variables

--- cloud/deploy/server/server_deployer.py
+++ cloud/deploy/server/server_deployer.py
@@ -1,13 +1,13 @@
 from deploy.common.base import BaseDeployer
+from deploy.common.mixins.package_manager_mixin import PackageManagerMixin
 from deploy.common.mixins.docker_mixin import DockerMixin
 from deploy.common.mixins.cloud_mixin import CloudMixin
 from deploy.common.mixins.bashrc_mixin import BashrcMixin
 
-class ServerDeployer(BaseDeployer, DockerMixin, CloudMixin, BashrcMixin):
+class ServerDeployer(BaseDeployer, PackageManagerMixin, DockerMixin, CloudMixin, BashrcMixin):
+    PACKAGE_MANAGER = "poetry"
+    PACKAGE_PATH = "server"
     IMAGE_NAME = "server-engine"
     DOCKERFILE = "cloud/Docker/server/server.Dockerfile"
     CONTEXT = "server"
     TAG = "gcr.io/my-project/server-engine:1.0.0"
-
-    def setup_os_env(self):
-        self.setup_bashrc(node=False, poetry=True)
