b440bedafbf4bdc13298aef7845e828605c57ba2
quichey <kylengo357@gmail.com>
2025-08-05 15:22:43 -0700

    retrieve old gcp code

--- cloud/deploy/cloud_providers_deployment/gcp_provider.py
+++ cloud/deploy/cloud_providers_deployment/gcp_provider.py
@@ -2,8 +2,16 @@ from .base_provider import BaseCloudProvider
 
 class GoogleCloudProvider(BaseCloudProvider):
     def get_build_cmd(self, dockerfile, package_path, tag):
-        raise NotImplementedError
+        return [
+            "gcloud", "builds", "submit", package_path,
+            "--tag", tag,
+            "--gcs-log-dir", "gs://my-logs"  # optional
+        ]
 
     def get_run_cmd(self, image_name, tag):
-        raise NotImplementedError
+        return [
+            "gcloud", "run", "deploy", image_name,
+            "--image", tag,
+            "--platform", "managed",
+        ]
 
