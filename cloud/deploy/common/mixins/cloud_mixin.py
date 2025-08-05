ca24f223e6e26163947f0ea1004b09167c38cc76
quichey <kylengo357@gmail.com>
2025-08-05 15:00:12 -0700

    connect pieces a bit

--- cloud/deploy/common/mixins/cloud_mixin.py
+++ cloud/deploy/common/mixins/cloud_mixin.py
@@ -1,6 +1,11 @@
 import subprocess
 
+from cloud_providers_deployment import get_provider_class
+
 class CloudMixin:
+    def __init__(self, provider_name):
+        self.provider = get_provider_class(provider_name)()
+
     def build_docker_image_cloud(self, image_name: str, dockerfile: str, package_path: str, tag: str):
         print(f"[CloudMixin] Building and submitting cloud image {image_name}")
         # Submit cloud build
