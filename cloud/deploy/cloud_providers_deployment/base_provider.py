b1ce536c0ca51861beaaf4ee28668d40285268c5
root <root@DESKTOP-F4B1KO7.localdomain>
2025-08-05 15:00:12 -0700

    connect pieces a bit

--- cloud/deploy/cloud_providers_deployment/base_provider.py
+++ cloud/deploy/cloud_providers_deployment/base_provider.py
@@ -1,20 +1,6 @@
-from abc import ABC, abstractmethod
+class BaseCloudProvider:
+    def deploy_app(self, *args, **kwargs):
+        raise NotImplementedError
 
-class BaseProvider(ABC):
-    PATH_PROJECT_ROOT = "../.."
-    PATH_PROJECT_DOCKER = "../Docker"
-
-    @abstractmethod
-    def fetch_services(self):
-        """Fetch a list of running services/instances."""
-        pass
-
-    @abstractmethod
-    def fetch_costs(self, service):
-        """Fetch the cost details for a specific service."""
-        pass
-
-    @abstractmethod
-    def shut_down(self, service):
-        """Shut down the given service/instance."""
-        pass
+    def destroy(self, *args, **kwargs):
+        raise NotImplementedError
