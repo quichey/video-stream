f320f5873d71be8ff369aa25ad9c2ed94c592a7c
quichey <kylengo357@gmail.com>
2025-08-05 21:34:28 -0700

    use .env file and do cleaner way of passing along context

--- cloud/deploy/common/mixins/cloud_mixin.py
+++ cloud/deploy/common/mixins/cloud_mixin.py
@@ -3,18 +3,14 @@ import subprocess
 from cloud_providers_deployment import get_provider_class
 
 class CloudMixin:
-    def __init__(self, provider_name, **kwargs):
-        self.provider = get_provider_class(provider_name)()
-        self.acr_name=kwargs["acr_name"]
-        self.resource_group=kwargs["resource_group"]
-        self.environment_name=kwargs["environment_name"]
+    def __init__(self, provider_name, context):
+        self.provider = get_provider_class(provider_name)(context)
 
     def build_docker_image_cloud(self, image_name: str, dockerfile: str, package_path: str, tag: str):
         cloud_cmd = self.provider.get_build_cmd(
             dockerfile,
             package_path,
             tag,
-            acr_name=self.acr_name,
         )
         print(f"[CloudMixin] Building and submitting cloud image {image_name}")
         # Submit cloud build
@@ -27,9 +23,6 @@ class CloudMixin:
         cloud_cmd = self.provider.get_run_cmd(
             image_name,
             tag,
-            acr_name=self.acr_name,
-            resource_group=self.resource_group,
-            environment_name=self.environment_name,
         )
         print(f"[CloudMixin] Deploying {image_name} to Cloud Run...")
         subprocess.run(
