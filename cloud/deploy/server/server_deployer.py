fa51efc005aff5eeba367b78b1215ca07f6c4267
quichey <kylengo357@gmail.com>
2025-08-05 22:43:35 -0700

    refactor chain of communication

--- cloud/deploy/server/server_deployer.py
+++ cloud/deploy/server/server_deployer.py
@@ -1,13 +1,7 @@
 from common.base import BaseDeployer
-from common.mixins.package_manager_mixin import PackageManagerMixin
-from common.mixins.docker_mixin import DockerMixin
-from common.mixins.cloud_mixin import CloudMixin
-from common.mixins.bashrc_mixin import BashrcMixin
 
-class ServerDeployer(BaseDeployer, PackageManagerMixin, DockerMixin, CloudMixin, BashrcMixin):
+class ServerDeployer(BaseDeployer):
     CONTEXT = "server"
     PACKAGE_MANAGER = "poetry"
     PACKAGE_PATH = f"{BaseDeployer.PATH_PROJECT_ROOT}/{CONTEXT}"
-    IMAGE_NAME = f"{CONTEXT}-engine"
     DOCKERFILE = f"{BaseDeployer.PATH_PROJECT_DOCKER}/{CONTEXT}/{CONTEXT}.Dockerfile"
-    TAG = f"gcr.io/my-project/{CONTEXT}-engine:1.0.0"
