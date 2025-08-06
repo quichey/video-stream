04b66149236ed62bcfbc60c758fb5f3c8f153b99
quichey <kylengo357@gmail.com>
2025-08-05 19:09:04 -0700

    parameterized the Azure cloud things -- want to flesh it out more

--- cloud/deploy/common/mixins/cloud_mixin.py
+++ cloud/deploy/common/mixins/cloud_mixin.py
@@ -3,11 +3,19 @@ import subprocess
 from cloud_providers_deployment import get_provider_class
 
 class CloudMixin:
-    def __init__(self, provider_name):
+    def __init__(self, provider_name, **kwargs):
         self.provider = get_provider_class(provider_name)()
+        self.acr_name=kwargs["acr_name"]
+        self.resource_group=kwargs["resource_group"]
+        self.environment_name=kwargs["environment_name"]
 
     def build_docker_image_cloud(self, image_name: str, dockerfile: str, package_path: str, tag: str):
-        cloud_cmd = self.provider.get_build_cmd(dockerfile, package_path, tag)
+        cloud_cmd = self.provider.get_build_cmd(
+            dockerfile,
+            package_path,
+            tag,
+            acr_name=self.acr_name,
+        )
         print(f"[CloudMixin] Building and submitting cloud image {image_name}")
         # Submit cloud build
         subprocess.run(
@@ -16,7 +24,13 @@ class CloudMixin:
         )
 
     def cloud_deploy(self, image_name: str, tag: str):
-        cloud_cmd = self.provider.get_run_cmd(image_name, tag)
+        cloud_cmd = self.provider.get_run_cmd(
+            image_name,
+            tag,
+            acr_name=self.acr_name,
+            resource_group=self.resource_group,
+            environment_name=self.environment_name,
+        )
         print(f"[CloudMixin] Deploying {image_name} to Cloud Run...")
         subprocess.run(
             cloud_cmd,
