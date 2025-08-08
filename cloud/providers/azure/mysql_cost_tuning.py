a75222e7f88cccc6ec735f1ee98007f3ed283452
root <root@DESKTOP-F4B1KO7.localdomain>
2025-08-08 13:41:31 -0700

    created Azure Container Registry

---
+++ cloud/providers/azure/mysql_cost_tuning.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+from azure import azure_cli
+
+load_dotenv(dotenv_path="./azure/.env")
+
+azure_cli_helper = azure_cli.AzureCLIHelper(
+    resource_group=os.environ.get("RESOUCE_GROUP_CENTRAL", 'blah')
+)
