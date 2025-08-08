6d65beaf8f6bf9a867e8728507a23638e393c8a3
root <root@DESKTOP-F4B1KO7.localdomain>
2025-08-08 13:48:08 -0700

    acr_name

--- cloud/providers/azure/mysql_cost_tuning.py
+++ cloud/providers/azure/mysql_cost_tuning.py
@@ -6,5 +6,6 @@ from azure import azure_cli
 load_dotenv(dotenv_path="./azure/.env")
 
 azure_cli_helper = azure_cli.AzureCLIHelper(
-    resource_group=os.environ.get("RESOUCE_GROUP_CENTRAL", 'blah')
+    resource_group=os.environ.get("RESOUCE_GROUP_CENTRAL", 'blah'),
+    acr_name=os.environ.get("CONTAINER_REGISTRY_NAME", 'blah'),
 )
