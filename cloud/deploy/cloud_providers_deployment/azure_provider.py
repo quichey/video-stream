9f86c9d8d09ef2ccc38adae7f90c4c3d8f4a1fee
quichey <kylengo357@gmail.com>
2025-08-11 16:12:48 -0700

    fixed pathing of sys import thing

--- cloud/deploy/cloud_providers_deployment/azure_provider.py
+++ cloud/deploy/cloud_providers_deployment/azure_provider.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 from providers.azure.azure_cli import AzureCLIHelper
 from .base_provider import BaseCloudProvider
 
