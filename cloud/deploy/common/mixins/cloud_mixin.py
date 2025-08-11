af248f5c056df42991ceac681b00dd63e7ce7a3e
root <root@DESKTOP-F4B1KO7.localdomain>
2025-08-11 16:35:10 -0700

    fixed some errors

--- cloud/deploy/common/mixins/cloud_mixin.py
+++ cloud/deploy/common/mixins/cloud_mixin.py
@@ -4,13 +4,13 @@ from cloud_providers_deployment import get_provider_class
 
 class CloudMixin:
     def __init__(self, provider_name, context):
+        self.context = context
         self.provider = get_provider_class(provider_name)(context)
 
-    def build_docker_image_cloud(self, dockerfile: str, package_path: str, tag: str):
+    def build_docker_image_cloud(self, dockerfile: str, package_path: str):
         cloud_cmd = self.provider.get_build_cmd(
             dockerfile,
             package_path,
-            tag,
         )
         print(f"[CloudMixin] Building and submitting cloud image {self.context}")
         # Submit cloud build
