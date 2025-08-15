4315b6d97826a8373a9a45045418a6c3418e659a
quichey <kylengo357@gmail.com>
2025-08-14 17:46:01 -0700

    add in provider hooks

--- cloud/deploy/cloud_providers_deployment/base_provider.py
+++ cloud/deploy/cloud_providers_deployment/base_provider.py
@@ -2,6 +2,18 @@ from abc import ABC, abstractmethod
 
 class BaseCloudProvider(ABC):
     @abstractmethod
+    def get_repo_name(self):
+        pass
+
+    @abstractmethod
+    def get_image_tag_base(self, repo_name):
+        pass
+
+    @abstractmethod
+    def get_latest_image(self, image_tag_base, repo_name):
+        pass
+
+    @abstractmethod
     def get_build_cmd(self, dockerfile, package_path, tag):
         pass
 
--- cloud/deploy/cloud_providers_deployment/gcp_provider.py
+++ cloud/deploy/cloud_providers_deployment/gcp_provider.py
@@ -11,6 +11,15 @@ class GoogleCloudProvider(BaseCloudProvider):
         self.tag = f"gcr.io/my-project/{context}-engine:1.0.0"
         return
 
+    def get_repo_name(self):
+        pass
+
+    def get_image_tag_base(self, repo_name):
+        pass
+
+    def get_latest_image(self, image_tag_base, repo_name):
+        pass
+
     def get_build_cmd(self, dockerfile, package_path, tag):
         return [
             "gcloud", "builds", "submit", package_path,
