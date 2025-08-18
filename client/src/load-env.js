af0cd352dcba3623493d1469bedf4b3e58198967
quichey <kylengo357@gmail.com>
2025-08-18 12:59:10 -0700

    fix load-env.js

--- client/src/load-env.js
+++ client/src/load-env.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const dotenv = require('dotenv');
 
-['.env.base', '.env.production', '.env.cloud'].forEach(file => {
+['.env.base', '.env.production', '.env.cloud', 'env/azure/.env'].forEach(file => {
   if (fs.existsSync(file)) {
     dotenv.config({ path: file });
   }
