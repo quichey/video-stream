a5885f10dd1aa075cc1d40ebb09124a7f5e6ff90
quichey <kylengo357@gmail.com>
2025-08-18 17:46:26 -0700

    add some base methods to Auth class

--- server/auth/google/google_auth.py
+++ server/auth/google/google_auth.py
@@ -1,9 +1,38 @@
+from authlib.integrations.flask_client import OAuth
+
 from auth.auth import Auth
 
 class GoogleAuth(Auth):
+    GOOGLE_CLIENT_ID = ""
+    GOOGLE_CLIENT_SECRET = ""
+    def __init__(self, app):
+        self.oauth = OAuth(app)
+        self.google = self.oauth.register(
+            name='google',
+            client_id=self.GOOGLE_CLIENT_ID,
+            client_secret=self.GOOGLE_CLIENT_SECRET,
+            access_token_url='https://accounts.google.com/o/oauth2/token',
+            authorize_url='https://accounts.google.com/o/oauth2/auth',
+            api_base_url='https://www.googleapis.com/oauth2/v1/',
+            client_kwargs={'scope': 'openid email profile'}
+        )
+        return
+    
+    def authorize(self):
+        token = self.google.authorize_access_token()
+        resp = self.google.get('userinfo')
+        user_info = resp.json()
+        return {
+            "token": token,
+            user_info: user_info,
+        }
+    
     def register(self, user_info):
+        self.authorize()
         pass
     def login(self, user_info):
+        self.authorize()
         pass
     def logout(self, user_info):
+        self.authorize()
         pass
