29fe34c8e4277aab7f3234bd3aae07cc8b1683ff
root <root@DESKTOP-F4B1KO7.localdomain>
2025-08-18 17:29:22 -0700

    make Auth an ABC class

--- server/auth/auth.py
+++ server/auth/auth.py
@@ -1,7 +1,12 @@
-class Auth():
+from abc import ABC, abstractmethod
+
+class Auth(ABC):
+    @abstractmethod
     def register(self, user_info):
         pass
+    @abstractmethod
     def login(self, user_info):
         pass
+    @abstractmethod
     def logout(self, user_info):
         pass
