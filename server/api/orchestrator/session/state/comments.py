800f3d7ddff2697f21f21bc58bd508f7e28a81b7
quichey <kylengo357@gmail.com>
2025-08-19 19:07:19 -0700

    fix up state/comments.py

--- server/api/orchestrator/session/state/comments.py
+++ server/api/orchestrator/session/state/comments.py
@@ -1,5 +1,5 @@
-from dataclasses import dataclass
-from typing import Optional
+
+from sqlalchemy import select
 
 from state.state_module import StateModule
 
@@ -9,14 +9,21 @@ COMMENTS_NEXT_PAGE_SIZE = 10
 
 
 class Comments(StateModule):
-    offset: int = 0
+    page_number = 0
     limit: int = COMMENTS_FIRST_PAGE_SIZE
-    next_page: bool = False
     video_id: int = None
 
     def __init__(self, video_id):
         #self.on_event("load_first_page_of_comments", self.load_first_page)
         self.video_id = video_id
+
+    @property
+    def offset(self):
+        if self.page_number == 0:
+            return 0
+        else:
+            return COMMENTS_FIRST_PAGE_SIZE + (self.page_number - 1) * COMMENTS_NEXT_PAGE_SIZE
+    
 
 
     """
@@ -26,30 +33,22 @@ class Comments(StateModule):
         using large OFFSET values can lead to slower queries, especially on large tables,
         as the database needs to scan through a large number of rows before applying the LIMIT. 
     """
-    def get_comments(self, session_info, page_number=0, page_size=50):
-        limit = page_size
-        offset = page_size * page_number # need to change this later to make up for initial page w/different size
-
-        if session_info is not None:
-            current_state_of_comments = self.session_manager.get_state(session_info, "video", "comments")
-            offset = current_state_of_comments.offset
-            limit = current_state_of_comments.limit
-            #print(f"\n\n offset: {offset} \n\n")
-            #print(f"\n\n limit: {limit} \n\n")
+    def get_comments(self, request):
+        limit = self.limit
+        offset = self.offset
 
         data = []
         with self.engine.connect() as conn:
             comments_table = self.metadata_obj.tables["comments"]
             users_table = self.metadata_obj.tables["users"]
 
-            current_video_state = self.session_manager.get_state(session_info, "video")
             subquery_select_cols = [comments_table.c.comment, comments_table.c.user_id]
             subquery = select(
                 *subquery_select_cols
             ).select_from(
                 comments_table
             ).where(
-                comments_table.c.video_id == current_video_state.id
+                comments_table.c.video_id == self.video_id
             ).cte("comments_one_video")
 
             select_cols = [subquery.c.comment, users_table.c.name]
@@ -77,20 +76,6 @@ class Comments(StateModule):
                 }
                 data.append(comment_data_point)
 
-            self.session_manager.update_state(session_info, "video", "offset", new_offset, "comments")
-
-        return data
-    
-    def load_next_page(self):
-        pass
-
-        # regression test
-    def load_first_page(self):
-
-        # TODO: fetch comments?
-        # update self.offset
+        self.page_number += 1
         self.limit = COMMENTS_NEXT_PAGE_SIZE
-        self.next_page = True
-        return {
-            "next_page"
-        }
+        return data
