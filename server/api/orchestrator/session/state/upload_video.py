d2fd59f7212cb7edbf4cae9b9dc5ea1502a6e982
quichey <kylengo357@gmail.com>
2025-08-20 15:46:55 -0700

    move video_upload code into correct files -- need to remember what it did and fix

--- server/api/orchestrator/session/state/upload_video.py
+++ server/api/orchestrator/session/state/upload_video.py
@@ -1,5 +1,6 @@
 from state.state_module import StateModule
 
+from api.util.request_data import extract_video_file_info
 
 VIDEO_UPLOAD_PAGE_SIZE = 6400000
 
@@ -9,16 +10,13 @@ class VideoUpload(StateModule):
     byte_stream: bytes
     is_done: bool
 
-    def __init__(self):
-        is_done=False
-        if len(video_upload_info.bytes) < VIDEO_UPLOAD_PAGE_SIZE:
-            is_done = True
-        user_state.video_upload = VideoUpload(
-            name=video_upload_info.name,
-            byte_stream=video_upload_info.bytes,
-            is_done=is_done
-        )
-        self.current_state[token] = user_state
+    def __init__(self, request, response):
+        video_file_info = extract_video_file_info(request=request)
+        self.name = video_file_info["name"]
+        self.byte_stream = video_file_info["byte_stream"]
+        self.is_done = video_file_info["is_done"]
+
+        
         return
 
 
@@ -26,43 +24,46 @@ class VideoUpload(StateModule):
     as of now, largest sample video works w/one server request
     but most likely i think need multiple for multiple gig sized files
     """
-    def video_upload(self, existing_session_info, video_upload_info):
-        token = existing_session_info
-        user_state = self.current_state[token]
-
-        if user_state.video_upload is None:
-            video_upload_session = self.start_video_upload(
-                existing_session_info=existing_session_info,
-                video_upload_info=video_upload_info,
-            )
-        else:
-            video_upload_session = self.continue_video_upload(
-                existing_session_info=existing_session_info,
-                video_upload_info=video_upload_info,
-            )
-
-        return video_upload_session
+    def video_upload(self, request, response):
+        #token = existing_session_info
+        #user_state = self.current_state[token]
+#
+        #if user_state.video_upload is None:
+        #    video_upload_session = self.start_video_upload(
+        #        existing_session_info=existing_session_info,
+        #        video_upload_info=video_upload_info,
+        #    )
+        #else:
+        #    video_upload_session = self.continue_video_upload(
+        #        existing_session_info=existing_session_info,
+        #        video_upload_info=video_upload_info,
+        #    )
+#
+        #return video_upload_session
+        return
     
-    def start_video_upload(self, existing_session_info, video_upload_info):
-        token = existing_session_info
-        user_state = self.current_state[token]
-        is_done=False
-        if len(video_upload_info.bytes) < VIDEO_UPLOAD_PAGE_SIZE:
-            is_done = True
-        user_state.video_upload = VideoUpload(
-            name=video_upload_info.name,
-            byte_stream=video_upload_info.bytes,
-            is_done=is_done
-        )
-        self.current_state[token] = user_state
-        return user_state.video_upload
+    def start_video_upload(self, request, response):
+        #token = existing_session_info
+        #user_state = self.current_state[token]
+        #is_done=False
+        #if len(self.bytes) < VIDEO_UPLOAD_PAGE_SIZE:
+        #    is_done = True
+        #user_state.video_upload = VideoUpload(
+        #    name=video_upload_info.name,
+        #    byte_stream=video_upload_info.bytes,
+        #    is_done=is_done
+        #)
+        #self.current_state[token] = user_state
+        #return user_state.video_upload
+        return
     
-    def continue_video_upload(self, existing_session_info, video_upload_info):
-        token = existing_session_info
-        user_state = self.current_state[token]
-        user_state.video_upload.byte_stream += video_upload_info.bytes
-        if len(video_upload_info.bytes) < VIDEO_UPLOAD_PAGE_SIZE:
-            user_state.video_upload.is_done = True
-        return user_state.video_upload
+    def continue_video_upload(self, request, response):
+        #token = existing_session_info
+        #user_state = self.current_state[token]
+        #user_state.video_upload.byte_stream += video_upload_info.bytes
+        #if len(video_upload_info.bytes) < VIDEO_UPLOAD_PAGE_SIZE:
+        #    user_state.video_upload.is_done = True
+        #return user_state.video_upload
+        return
    
     
--- server/api/util/request_data.py
+++ server/api/util/request_data.py
@@ -32,3 +32,45 @@ def extract_temp_cookie(request):
 def has_temp_cookie(request):
     cookie = extract_temp_cookie(request=request)
     return cookie is not None
+
+
+
+"""
+TODO: at some point
+implement way to handle chunked data
+"""
+def extract_video_file_info(request):
+    user_info = extract_user_info(request)
+    #print(f"\n\n reached extract_video_file_info \n\n")
+    form_data = json.loads(request.data)
+    video_file_info = {}
+    if "file_info" in form_data:
+        file_info = form_data['file_info']
+        #print(f"\n\n file_info: {file_info} \n\n")
+        file_stream = dict(file_info["file_stream"])
+        #print(f"\n\n len(file_stream.keys()): {len(file_stream.keys())} \n\n")
+        file_name = file_info["name"]
+        # file being loaded as str
+        # convert to something consumable by bytes() construct
+        video_file_info["bytes"] = file_stream
+        video_file_info["name"] = file_name
+        #video_file_info["user_id"] = user_info["id"]
+        video_file_info["user_id"] = 1
+        video_file_info["upload_date"] = "now"
+    video_file_info["bytes"] = decode_video(video_file_info["bytes"])
+    return video_file_info
+
+
+
+def decode_video(self, file_stream):
+    as_array = []
+    for _, b in file_stream.items():
+        #print(f"\n\n bytes(b): {bytes(b)} \n\n")
+        as_array.append(b)
+    #print(f"\n\n as_array: {as_array} \n\n")
+    #print(f"\n\n len(as_array): {len(as_array)} \n\n")
+    as_bytes = bytes(as_array)
+    #print(f"\n\n as_bytes: {as_bytes} \n\n")
+    #print(f"\n\n len(as_bytes): {len(as_bytes)} \n\n")
+    return as_bytes
+    #return as_bytes.decode('utf-8', errors='ignore')
