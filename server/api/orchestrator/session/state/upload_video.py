4f51b135f0256830144f48eb69a4566f2ad628cd
root <root@DESKTOP-F4B1KO7.localdomain>
2025-08-22 16:07:35 -0700

    migrate store_video from Cache.py to upload_video.py

--- server/api/orchestrator/session/state/upload_video.py
+++ server/api/orchestrator/session/state/upload_video.py
@@ -1,3 +1,10 @@
+from sqlalchemy.orm import Session
+from datetime import datetime
+
+from db.Schema import Video
+from db.Schema.Video import VideoFileManager
+from api.Routers import VideoUpload
+
 from api.orchestrator.session.state.state_module import StateModule
 
 from api.util.request_data import extract_video_file_info
@@ -26,45 +33,80 @@ class VideoUpload(StateModule):
     but most likely i think need multiple for multiple gig sized files
     """
     def video_upload(self, request, response):
-        #token = existing_session_info
-        #user_state = self.current_state[token]
-#
-        #if user_state.video_upload is None:
-        #    video_upload_session = self.start_video_upload(
-        #        existing_session_info=existing_session_info,
-        #        video_upload_info=video_upload_info,
-        #    )
-        #else:
-        #    video_upload_session = self.continue_video_upload(
-        #        existing_session_info=existing_session_info,
-        #        video_upload_info=video_upload_info,
-        #    )
-#
-        #return video_upload_session
+        token = existing_session_info
+        user_state = self.current_state[token]
+
+        if user_state.video_upload is None:
+            video_upload_session = self.start_video_upload(
+                existing_session_info=existing_session_info,
+                video_upload_info=video_upload_info,
+            )
+        else:
+            video_upload_session = self.continue_video_upload(
+                existing_session_info=existing_session_info,
+                video_upload_info=video_upload_info,
+            )
+
+        return video_upload_session
         return
     
     def start_video_upload(self, request, response):
-        #token = existing_session_info
-        #user_state = self.current_state[token]
-        #is_done=False
-        #if len(self.bytes) < VIDEO_UPLOAD_PAGE_SIZE:
-        #    is_done = True
-        #user_state.video_upload = VideoUpload(
-        #    name=video_upload_info.name,
-        #    byte_stream=video_upload_info.bytes,
-        #    is_done=is_done
-        #)
-        #self.current_state[token] = user_state
-        #return user_state.video_upload
+        token = existing_session_info
+        user_state = self.current_state[token]
+        is_done=False
+        if len(self.bytes) < VIDEO_UPLOAD_PAGE_SIZE:
+            is_done = True
+        user_state.video_upload = VideoUpload(
+            name=video_upload_info.name,
+            byte_stream=video_upload_info.bytes,
+            is_done=is_done
+        )
+        self.current_state[token] = user_state
+        return user_state.video_upload
         return
     
     def continue_video_upload(self, request, response):
-        #token = existing_session_info
-        #user_state = self.current_state[token]
-        #user_state.video_upload.byte_stream += video_upload_info.bytes
-        #if len(video_upload_info.bytes) < VIDEO_UPLOAD_PAGE_SIZE:
-        #    user_state.video_upload.is_done = True
-        #return user_state.video_upload
+        token = existing_session_info
+        user_state = self.current_state[token]
+        user_state.video_upload.byte_stream += video_upload_info.bytes
+        if len(video_upload_info.bytes) < VIDEO_UPLOAD_PAGE_SIZE:
+            user_state.video_upload.is_done = True
+        return user_state.video_upload
         return
    
     
+    def store_video(self, video_file_info: VideoUpload, session_info):
+        """
+        check if user has started a VideoUpload session
+        if not, start one,
+        else, continue.
+
+        if reached 0 bytes, store the video
+        """
+        video_upload_session = self.session_manager.video_upload(
+            session_info,
+            video_upload_info=video_file_info
+        )
+        if not video_upload_session.is_done:
+            return video_upload_session.is_done
+
+        #TODO: copy to client/public/videos folder
+        video = Video(
+            user_id=video_file_info.user_id,
+            file_dir=video_file_info.user_id,
+            file_name=video_file_info.name,
+            date_created=datetime.now(),
+            date_updated=datetime.now(),
+        )
+        manager = VideoFileManager()
+        manager.store_video(
+            video_record=video,
+            seeding_db=False,
+            byte_stream=video_upload_session.byte_stream
+        )
+        # also save to mysql db
+        with Session(self.engine) as session:
+            session.add(video)
+            session.commit()
+            
+        return video_upload_session.is_done
