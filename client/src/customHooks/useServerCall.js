283c7dc9a2942e2a1ca225e4191ef4db8348dba1
root <root@DESKTOP-F4B1KO7.localdomain>
2025-08-23 14:36:18 -0700

    refactor useServerCall.js

--- client/src/customHooks/useServerCall.js
+++ client/src/customHooks/useServerCall.js
@@ -1,34 +1,30 @@
-import * as React from "react";
-
+import { useCallback } from "react";
 import { HTTPContext } from "../pages";
+import { useContext } from "react";
+import { buildRequestBody } from "../api/httpUtils";
 
 export const useServerCall = () => {
-  const {serverURL, postRequestPayload} = React.useContext(HTTPContext);
-  const fetchServer = React.useCallback((route, onResponse, method="POST") => {
-      fetch(
-        `${serverURL}/${route}`,
-        {
-          method: method,
+  const { serverURL } = useContext(HTTPContext);
+
+  const fetchServer = useCallback(
+    async (route, httpParams = {}, method = "POST") => {
+      try {
+        const res = await fetch(`${serverURL}/${route}`, {
+          method,
           credentials: "include",
-          // may need to use POST later for adding params
-          // i think don't have to, could use query string
-          // POST is probably more secure cause body is probably encrypted
-          //method: "POST",
-          // body: JSON.stringify({ limit: 30 }),
-          // mode: "no-cors",
-          body: postRequestPayload
-        },
-      )
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        onResponse(json)
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  
-    }, [serverURL, postRequestPayload])
+          body: buildRequestBody(httpParams),
+        });
+
+        if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+        const data = await res.json();
+        return data; // caller can handle response
+      } catch (err) {
+        console.error("Server call failed", err);
+        throw err; // propagate error to caller
+      }
+    },
+    [serverURL]
+  );
 
   return fetchServer;
 };
