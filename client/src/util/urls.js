ca47574e1e85bd1c2d9764cf4d2f5b8e419163c7
root <root@DESKTOP-F4B1KO7.localdomain>
2025-08-22 21:39:22 -0700

    make get_storage_url agnostic of azure

--- client/src/util/urls.js
+++ client/src/util/urls.js
@@ -2,6 +2,8 @@ export function get_storage_url(deployEnv) {
     if (deployEnv === "local") {
         return process.env.PUBLIC_URL
     } else {
-        return `https://${process.env.REACT_APP_STORAGE_ACCOUNT_NAME}.blob.core.windows.net`
+        const name = process.env.REACT_APP_STORAGE_ACCOUNT_NAME
+        const suffix = process.env.REACT_APP_STORAGE_ACCOUNT_SUFFIX
+        return `https://${name}.${suffix}`
     }
 }
