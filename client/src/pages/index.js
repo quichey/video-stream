70a2cf3db495feac6b1286f5099a9335430bac2c
root <root@DESKTOP-F4B1KO7.localdomain>
2025-08-23 15:13:55 -0700

    migrate all of the contexts out of src/pages/index.js

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -9,78 +9,38 @@ import VideoUpload from "./video_upload";
 import Navbar from "./Navbar";
 
 
-export const ChannelContext = React.createContext(null);
-
-export const serverURL = process.env.REACT_APP_SERVER_APP_URL || process.env.REACT_APP_API_BASE;
-export const HTTPContext = React.createContext(null);
-
 export default function Pages() {
-  //const [userID, setUserID] = React.useState(0);
-  //const [userName, setUserName] = React.useState("users_name_0");
-
-  const [channelID, setChannelID] = React.useState(0);
-  const [channelName, setChannelName] = React.useState("users_name_0");
-  const [postRequestPayload, setPostRequestPayload] = React.useState(0);
-
-  React.useEffect(() => {
-    var payloadObject = {
-      //user_id: userID,
-      //user_name: userName,
-      video_id: videoID,
-      session_token: sessionStorage.getItem("tempSessionToken")
-    };
-    var payloadJSON = JSON.stringify(payloadObject);
-    setPostRequestPayload(payloadJSON);
-  //}, [userID, userName, videoID]);
-  }, [videoID]);
-
   return (
-    <ChannelContext.Provider
-      value={{
-        id: channelID,
-        setID: setChannelID,
-        name: channelName,
-        setName: setChannelName,
-      }}
-    >
-        <HTTPContext.Provider
-          value={{
-            serverURL: serverURL,
-            postRequestPayload: postRequestPayload,
-          }}
-        >
-          <BrowserRouter>
-            <Box
-              component="form"
-              sx={{
-                "& > :not(style)": { m: 1, width: "100%" },
-              }}
-              noValidate
-              display="flex"
-              flexDirection="column"
-              autoComplete="off"
-              style={{
-                width: "100%",
-              }}
-            >
-              <Navbar />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="watch">
-                  <Route path=":videoID" element={<Watch key={videoID}/>} />
-                </Route>
-                <Route path="channel">
-                  <Route path=":userID" element={<User />} />
-                </Route>
-                <Route
-                  index
-                  path="upload"
-                  element={<VideoUpload />}
-                />
-              </Routes>
-            </Box>
-          </BrowserRouter>
-        </HTTPContext.Provider>
-    </ChannelContext.Provider>
+    <BrowserRouter>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "100%" },
+        }}
+        noValidate
+        display="flex"
+        flexDirection="column"
+        autoComplete="off"
+        style={{
+          width: "100%",
+        }}
+      >
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="watch">
+            <Route path=":videoID" element={<Watch />} />
+          </Route>
+          <Route path="channel">
+            <Route path=":userID" element={<User />} />
+          </Route>
+          <Route
+            index
+            path="upload"
+            element={<VideoUpload />}
+          />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
