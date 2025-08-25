55bcd30f73b9ffdc075a966d64ef791497ae3c30
quichey <kylengo357@gmail.com>
2025-08-25 14:31:33 -0700

    local tests for improved infinite scroll of comments looks good

--- client/src/pages/watch/comments/index.js
+++ client/src/pages/watch/comments/index.js
@@ -3,18 +3,22 @@ import List from "@mui/material/List";
 
 import { useInfiniteScroll } from "../../../customHooks/useInfiniteScroll";
 
+import Comment from "./Comment";
+
 export default function Comments() {
   const [comments, setComments] = React.useState([]);
 
   const { sentinelRef, loading } = useInfiniteScroll({
     route: "getcomments",
-    handleData: (newComments) => setComments(prev => [...prev, ...newComments]),
+    handleData: (data) => setComments(prev => [...prev, ...data.comment_data]),
   });
 
   return (
     <div>
       <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-        {comments}
+        {comments.map(data => {
+          return <Comment comment={data.comment} user={data.user_name} />
+        })}
       </List>
       <div ref={sentinelRef} style={{ height: 1 }} />
       {loading && <div>Loading...</div>}
