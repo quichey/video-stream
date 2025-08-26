84df5dff2d7024a842429b60b506b8e3cd0f932f
root <root@DESKTOP-F4B1KO7.localdomain>
2025-08-26 15:46:17 -0700

    make session/index.js into SessionButton

--- client/src/pages/user/session/index.js
+++ client/src/pages/user/session/index.js
@@ -1,66 +1,27 @@
 import React, { useState } from "react";
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  TextField,
-  Button,
-  Typography,
-  Link,
-} from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
-function LoginDialog() {
-  const [open, setOpen] = useState(false);
+import IconButtonVS from "../../../components/IconButton";
+import SessionMenu from "./SessionMenu";
 
-  const handleLogin = () => {
-    // handle login logic here
-    setOpen(false);
+function SessionButton() {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
 
-  const handleRegister = () => {
-    // open registration dialog or redirect
-    console.log("Navigate to Register");
+  const handleClose = () => {
+    setAnchorEl(null);
   };
+  // TODO: use profiles ICON image as icon
 
   return (
-    <>
-      <Button variant="contained" onClick={() => setOpen(true)}>
-        Login
-      </Button>
-
-      <Dialog open={open} onClose={() => setOpen(false)}>
-        <DialogTitle>Login</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            label="Email"
-            type="email"
-            fullWidth
-          />
-          <TextField
-            margin="dense"
-            label="Password"
-            type="password"
-            fullWidth
-          />
-          <Typography variant="body2" mt={2}>
-            Don't have an account?{" "}
-            <Link component="button" onClick={handleRegister}>
-              Register
-            </Link>
-          </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => setOpen(false)}>Cancel</Button>
-          <Button variant="contained" onClick={handleLogin}>
-            Login
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
+    <div>
+      <IconButtonVS IconComponent={<AccountCircleIcon />} handleClick={handleClick} />
+      <SessionMenu handleClose={handleClose} anchorEl={anchorEl}/>
+    </ div>
   );
 }
 
-export default LoginDialog;
+export default SessionButton;
