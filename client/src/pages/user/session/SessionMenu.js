124e72248ac36c4fe6d5e6f8707d6d1df8c5fc4c
quichey <kylengo357@gmail.com>
2025-08-26 15:46:17 -0700

    make session/index.js into SessionButton

--- client/src/pages/user/session/SessionMenu.js
+++ client/src/pages/user/session/SessionMenu.js
@@ -4,25 +4,13 @@ import { Popover, Button, MenuItem, Typography } from "@mui/material";
 import Login from "./Login";
 import Logout from "./Logout";
 
-export default function SessionMenu() {
-  const [anchorEl, setAnchorEl] = useState(null);
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+export default function SessionMenu({ handleClose, anchorEl }) {
 
   const open = Boolean(anchorEl);
   const id = open ? "simple-popover" : undefined;
 
   return (
     <div>
-      <Button variant="contained" onClick={handleClick}>
-        Open Menu
-      </Button>
 
       <Popover
         id={id}
