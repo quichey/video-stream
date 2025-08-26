669dd5c9305c8fa52035d59462e3f06d87fff6f9
root <root@DESKTOP-F4B1KO7.localdomain>
2025-08-26 15:34:45 -0700

    add menuitems to SessionMenu

--- client/src/pages/user/session/Register.js
+++ client/src/pages/user/session/Register.js
@@ -2,22 +2,13 @@ import React, { useState } from "react";
 import { Popover, Button, MenuItem, Typography } from "@mui/material";
 
 export default function Register() {
-  const [anchorEl, setAnchorEl] = useState(null);
 
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
   };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const open = Boolean(anchorEl);
-  const id = open ? "simple-popover" : undefined;
 
   return (
     <div>
-        <MenuItem onClick={handleClose}>Register</MenuItem>
+        <MenuItem onClick={handleClick}>Register</MenuItem>
     </div>
   );
 }
