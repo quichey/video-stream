685c11b604f5076360a5b6d78b2487ec773c7401
root <root@DESKTOP-F4B1KO7.localdomain>
2025-08-27 04:35:58 -0700

    create AUTH_COOKIE field in UserSession

--- server/api/orchestrator/session/UserSession.py
+++ server/api/orchestrator/session/UserSession.py
@@ -4,9 +4,13 @@ class SecurityError(Exception):
     pass
 
 class UserSession(SessionBase):
+    AUTH_COOKIE = None
     def __init__(self, user_id: int):
         self.token = f"user-{user_id}"
         self.state = {}  # could be from DB/cache
+    
+    def generate_auth_cookie(self, request, response):
+        pass
 
     def get_token(self):
         return self.token
