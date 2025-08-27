bea1e79c414f363804367d5574020e7468822f51
quichey <kylengo357@gmail.com>
2025-08-27 04:49:41 -0700

    Add generate_auth_cookie to UserSession

---
+++ server/api/util/cookie.py
@@ -0,0 +1,20 @@
+import uuid
+
+
+def generate_uuid():
+        _uuid = str(uuid.uuid4())
+        return _uuid
+
+def generate_cookie(name, deployment, response):
+        cookie_id = generate_uuid()
+        IS_PRODUCTION = deployment is 'cloud'
+        response.set_cookie(
+            name,
+            cookie_id,
+            max_age=30*24*60*60,  # 30 days
+            httponly=True,
+            secure=IS_PRODUCTION,  # True in production (HTTPS), False locally
+            samesite='None' if IS_PRODUCTION else 'Lax',  # None for cross-site in prod, Lax for local
+            path='/'
+        )
+        return cookie_id
