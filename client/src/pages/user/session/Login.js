5e2c7265926dea9f3492c3b658c5b1466b693960
quichey <kylengo357@gmail.com>
2025-08-27 18:51:36 -0700

    fill in Login.js

--- client/src/pages/user/session/Login.js
+++ client/src/pages/user/session/Login.js
@@ -9,37 +9,64 @@ import {
   Link,
 } from "@mui/material";
 
+import { useServerCall } from "../../../customHooks/useServerCall";
+
 export default function Login() {
     const [open, setOpen] = useState(false);
+    const [name, setName] = useState(false);
+    const [password, setPassword] = useState(false);
+    const fetchData = useServerCall();
 
-  const handleLogin = () => {
-    // handle login logic here
-    setOpen(false);
-  };
+    const handleLogin = React.useCallback(() => {
+        // handle register logic here
+        // Do Server api
+        fetchData("login", (json) => {
+            const tempToken = json.session_token
+            sessionStorage.setItem("tempSessionToken", tempToken);
+            setOpen(false);
+        }, { user_name: name, password: password });
+      }, [fetchData, name, password]);
 
-  const handleClick = (event) => {
-  };
+    const handleClick = (event) => {
+        setOpen(true)
+    };
+  
+    const handleNameChange = React.useCallback((e) => {
+        if (e.target?.value !== undefined) {
+            setName(e.target.value)
+        }
+    }, [])
+
+    const handlePasswordChange = React.useCallback((e) => {
+        if (e.target?.value !== undefined) {
+            setPassword(e.target.value)
+        }
+    }, [])
 
 
-  return (
+    return (
     <div>
         <MenuItem onClick={handleClick}>Login</MenuItem>
         <Dialog open={open} onClose={() => setOpen(false)}>
         <DialogTitle>Login</DialogTitle>
         <DialogContent>
-          <TextField
+            <TextField
             autoFocus
             margin="dense"
-            label="Email"
-            type="email"
+            label="User Name"
+            type="text"
             fullWidth
-          />
-          <TextField
-            margin="dense"
-            label="Password"
-            type="password"
-            fullWidth
-          />
+            value={name}
+            onChange={handleNameChange}
+            />
+            <TextField
+                margin="dense"
+                label="Password"
+                type="password"
+                fullWidth
+                value={password}
+                onChange={handlePasswordChange}
+            />
         </DialogContent>
         <DialogActions>
           <Button onClick={() => setOpen(false)}>Cancel</Button>
@@ -49,5 +76,5 @@ export default function Login() {
         </DialogActions>
       </Dialog>
     </div>
-  );
+    );
 }
