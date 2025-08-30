2536b5db966b05142cbf8ead0c15ae02ef0aa415
root <root@DESKTOP-F4B1KO7.localdomain>
2025-08-30 16:15:52 -0700

    fill in store_file_in_public

--- server/api/orchestrator/storage/local_storage.py
+++ server/api/orchestrator/storage/local_storage.py
@@ -1,23 +1,41 @@
 import datetime
 import os
 import sys
+import shutil
 
 from api.orchestrator.storage.base_storage import BaseStorage
 
+
+CLIENT_PUBLIC_FOLDER = "../client/public"
+CLIENT_VIDEOS_FOLDER = f"{CLIENT_PUBLIC_FOLDER}/videos"
+CLIENT_IMAGES_FOLDER = f"{CLIENT_PUBLIC_FOLDER}/images"
 
 class LocalStorage(BaseStorage):
     CONTAINER_VIDEOS = "videos"                      # e.g. "videos"
     CONTAINER_IMAGES= "images"
 
-    def store_video(self, file_dir, file_name):
+    def store_video(self, file_dir, file_name, byte_stream):
         pass
 
-    def store_image(self, file_dir, file_name):
+    def store_image(self, file_dir, file_name, byte_stream):
         pass
 
-    def store_file_in_public(self, file_dir, file_name):
+    def store_file_in_public(self, file_dir, file_name, byte_stream, container):
         #TODO: understand what i did in db/Schema/Video/VideoFile.py
-        pass
+        file_location = file_dir
+        client_full_file_location = f"{CLIENT_PUBLIC_FOLDER}/{container}/{file_location}"
+        client_full_file_name = f"{client_full_file_location}/{file_name}"
+        user_folder_exists = os.path.exists(client_full_file_location)
+        if not user_folder_exists:
+            os.mkdir(client_full_file_location)
+
+        file_exists = os.path.exists(client_full_file_name)
+        if not file_exists:
+            #TODO: handle mp4 files
+            file = open(client_full_file_name, "wb")
+            file.write(byte_stream)
+            file.close()
+				
 
     def get_video_url(self, file_dir, file_name):
         pass
