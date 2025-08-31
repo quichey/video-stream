dae9c0bacead4e49f4c93f6ec75b4dc4c18d9ebe
quichey <kylengo357@gmail.com>
2025-08-30 19:28:32 -0700

    preview picture

--- client/src/pages/user/edit/Picture.js
+++ client/src/pages/user/edit/Picture.js
@@ -12,10 +12,18 @@ import ButtonVS from "../../../components/TextButton";
 
 export default function Picture() {
     const [fileBytes, setFileBytes] = React.useState(new Uint8Array(0));
+    const [fileName, setFileName] = React.useState(new Uint8Array(0));
+    const [preview, setPreview] = React.useState(null);
   const { id: loggedInUserID, iconFileName, iconSASURL } = React.useContext(UserContext);
   const fetchData = useServerCall()
 
   const onFileChange = (file) => {
+    const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result); // base64 string
+      };
+      reader.readAsDataURL(file);
+    setFileName(file.name)
     readFile(file, setFileBytes)
   }
 
@@ -23,8 +31,8 @@ export default function Picture() {
     fetchData("upload-profile-pic", (json) => {
         console.log(json)
     }, {
-        "user_id": loggedInUserID,
-        "file": fileBytes
+        "file_name": fileName,
+        "byte_stream": fileBytes
     })
   }
   return (
@@ -41,12 +49,18 @@ export default function Picture() {
         width: "100%",
       }}
     >
-        <UserIconImg
-            id={loggedInUserID}
-            userIcon={iconFileName}
-            userIconURL={iconSASURL}
-            length="50px"
-        />
+        {
+            preview ? (
+                <img src={preview} alt="Preview" style={{ width: 50, height: 50}}/>
+            ):
+                <UserIconImg
+                    id={loggedInUserID}
+                    userIcon={iconFileName}
+                    userIconURL={iconSASURL}
+                    length="50px"
+                />
+
+        }
         <FileUploadButton text="Change" onChange={onFileChange} />
         <p>
             Remove
