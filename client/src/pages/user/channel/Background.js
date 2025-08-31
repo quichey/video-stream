3a763d2c11dc5e9fca3a5d6c3e2a8590cee2e1d8
root <root@DESKTOP-F4B1KO7.localdomain>
2025-08-30 22:20:50 -0700

    create Placeholder.js

--- client/src/pages/user/channel/Background.js
+++ client/src/pages/user/channel/Background.js
@@ -1,27 +1,9 @@
 import * as React from "react";
-import { Box } from "@mui/material";
 
-import { ChannelContext } from "../../../contexts/ChannelContext";
+import Placeholder from "../../../components/Placeholder";
 
 export default function Background() {
-  const { id, name } = React.useContext(ChannelContext);
   return (
-    <Box
-      component="form"
-      sx={{
-        "& > :not(style)": { m: 1, width: "100%" },
-      }}
-      noValidate
-      display="flex"
-      flexDirection="column"
-      autoComplete="off"
-      style={{
-        width: "100%",
-      }}
-    >
-      <p>
-        Background photo
-      </p>
-    </Box>
+        <Placeholder label="Background: Pending Development" />
   );
 }
--- client/src/pages/user/edit/Name.js
+++ client/src/pages/user/edit/Name.js
@@ -1,40 +1,10 @@
 import * as React from "react";
-import { Box } from "@mui/material";
 
-import { UserContext } from "../../../contexts/UserContext";
-import { ChannelContext } from "../../../contexts/ChannelContext";
-
+import Placeholder from "../../../components/Placeholder";
 
 export default function Name() {
-  const { id: loggedInUserID } = React.useContext(UserContext);
-  const { id: channelID, name } = React.useContext(ChannelContext);
 
   return (
-    <Box
-      component="form"
-      sx={{
-        "& > :not(style)": { m: 1, width: "100%" },
-      }}
-      noValidate
-      display="flex"
-      flexDirection="column"
-      autoComplete="off"
-      style={{
-        width: "100%",
-      }}
-    >
-      <p>
-        Profile Icon
-      </p>
-      <p>
-        Name
-      </p>
-      <p>
-        @handle
-      </p>
-      <p>
-        Description
-      </p>
-    </Box>
+    <Placeholder label="Name: Pending Development" />
   );
 }
