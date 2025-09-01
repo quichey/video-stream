a08f1f35dde610a0fbecd072e5579aeddecc54ab
quichey <kylengo357@gmail.com>
2025-08-31 23:30:05 -0700

    get rid of 'broken' images from users with no pic

--- client/src/pages/user/session/index.js
+++ client/src/pages/user/session/index.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 import IconButtonVS from "../../../components/IconButton";
 import SessionMenu from "./SessionMenu";
 
 import { UserContext } from "../../../contexts/UserContext";
-import { get_storage_url } from "../../../util/urls";
+
+import UserIconImg from "../../../components/UserIconImg";
 
 function SessionButton() {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -18,21 +18,16 @@ function SessionButton() {
   const handleClose = () => {
     setAnchorEl(null);
   };
-  // TODO: use profiles ICON image as icon
-  const img_url = get_storage_url("images", id, iconFileName, iconSASURL)
-  const has_profile_pic = id !== undefined && iconFileName !== undefined;
 
   return (
     <div>
       <IconButtonVS handleClick={handleClick} >
-        {
-            has_profile_pic ? <img 
-                src={img_url} 
-                alt="icon" 
-                style={{ width: 24, height: 24 }} // size to match MUI icons
-            /> : <AccountCircleIcon />
-        }
-        
+        <UserIconImg
+          id={id}
+          userIcon={iconFileName}
+          userIconURL={iconSASURL}
+          length="24px"
+        />
       </IconButtonVS>
       <SessionMenu handleClose={handleClose} anchorEl={anchorEl}/>
     </ div>
