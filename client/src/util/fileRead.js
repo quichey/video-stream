30768c1ef4b1584a93f65365f8f971ace9aae718
quichey <kylengo357@gmail.com>
2025-08-31 23:10:54 -0700

    fix change -> remove -> change bug

--- client/src/util/fileRead.js
+++ client/src/util/fileRead.js
@@ -1,3 +1,11 @@
+
+function joinByteArrays(array1, array2) {
+    const mergedArray = new Uint8Array(array1.length + array2.length);
+    mergedArray.set(array1, 0);
+    mergedArray.set(array2, array1.length);
+    return mergedArray;
+}
+
 export const readFile = async (fileObject, setFile) => {
     if (!fileObject) {
       //console.log("No file selected.");
@@ -5,23 +13,17 @@ export const readFile = async (fileObject, setFile) => {
     }
     const stream = fileObject.stream();
     const reader = stream.getReader();
+    let fullFileBytes = new Uint8Array(0)
     while (true) {
       const { done, value } = await reader.read();
       if (done) {
         //console.log("Stream reading complete.");
+        setFile(fullFileBytes)
         return;
       }
       // Process the chunk of data
       //console.log("Chunk:", value);
-      setFile((prevFileArray) => {
-        function joinByteArrays(array1, array2) {
-          const mergedArray = new Uint8Array(array1.length + array2.length);
-          mergedArray.set(array1, 0);
-          mergedArray.set(array2, array1.length);
-          return mergedArray;
-        }
-        return joinByteArrays(prevFileArray, value);
-      });
+      fullFileBytes = joinByteArrays(fullFileBytes, value)
     }
 
 }
