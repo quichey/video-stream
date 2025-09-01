628e4cc75588918e713e6afc6d76af6067634871
quichey <kylengo357@gmail.com>
2025-09-01 03:17:51 -0700

    add --env arg choices

--- cloud/deploy/deploy_machines.py
+++ cloud/deploy/deploy_machines.py
@@ -16,7 +16,12 @@ if __name__ == "__main__":
 
     # cloud provider option
     parser.add_argument("--cloud_provider", default="Azure", help="Cloud Provider")
-    parser.add_argument("--env", default="dev", help="Deployment env (Prod/Stage/Dev/Test)")
+    parser.add_argument(
+        "--env",
+        default="dev",
+        choices=["prod", "stage", "dev", "test"],
+        help="Deployment environment (prod/stage/dev/test)"
+    )
 
     parser.add_argument("--client", action="store_true", help="Deploy client service")
     parser.add_argument("--server", action="store_true", help="Deploy server service")
