7a4b70558688687c95841091843ec79e3aa1e9f4
root <root@DESKTOP-F4B1KO7.localdomain>
2025-08-31 17:42:36 -0700

    fix sidebar icons

--- client/src/pages/sidebar/Music.js
+++ client/src/pages/sidebar/Music.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-import HomeIcon from "@mui/icons-material/Home";
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 import { NavLink } from "react-router-dom";
 
 export default function Music({ collapsed }) {
   return (
-    <ListItemButton component={NavLink} to="/">
+    <ListItemButton component={NavLink} to="/music">
       <ListItemIcon>
-        <HomeIcon />
+        <LibraryMusicIcon />
       </ListItemIcon>
-      {!collapsed && <ListItemText primary="Home" />}
+      {!collapsed && <ListItemText primary="Music" />}
     </ListItemButton>
   );
 }
