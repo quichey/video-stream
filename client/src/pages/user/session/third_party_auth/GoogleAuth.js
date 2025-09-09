780bf0ee8fe7fbb485fc8e044971f948a457aee4
quichey <kylengo357@gmail.com>
2025-09-08 21:41:26 -0700

    fix get google route

--- client/src/pages/user/session/third_party_auth/GoogleAuth.js
+++ client/src/pages/user/session/third_party_auth/GoogleAuth.js
@@ -1,29 +1,12 @@
 import React from "react";
 import GoogleIcon from "@mui/icons-material/Google";
 
-import { useServerCall } from "../../../../customHooks/useServerCall";
-import { UserContext } from "../../../../contexts/UserContext";
 import IconButtonVS from "../../../../components/IconButton";
 
 export default function GoogleAuth() {
-  const fetchData = useServerCall();
-  const { setName, setID, setIconFileName, setIconSASURL } =
-    React.useContext(UserContext);
-
-  const handleClick = React.useCallback(
-    (event) => {
-      fetchData("google/login", (json) => {
-        // TODO: this data comes in on reload, right? so this setting logic  belongs in useLoadSession?
-        const tempToken = json.session_token;
-        sessionStorage.setItem("tempSessionToken", tempToken);
-        setName(json.user_data.name);
-        setID(json.user_data.id);
-        setIconFileName(json.user_data.profile_icon);
-        setIconSASURL(json.user_data.profile_icon_sas_url);
-      });
-    },
-    [fetchData, setID, setIconFileName, setIconSASURL, setName],
-  );
+  const handleClick = React.useCallback((event) => {
+    window.location.href = "http://localhost:5000/google/login";
+  }, []);
 
   return (
     <IconButtonVS handleClick={handleClick}>
