13ab3d2b0099c0613a7b86f8eefbc63e8c6c46de
root <root@DESKTOP-F4B1KO7.localdomain>
2025-09-16 21:06:15 -0700

    create watchVideo test func

--- testing/e2e/user_session/user_session_spec.js
+++ testing/e2e/user_session/user_session_spec.js
@@ -3,7 +3,7 @@ import { logInTests, logOut, registerUser, openSessionMenu } from ".";
 
 export function runSessionTests({ username, password }) {
     openSessionMenu();
-  registerUser({username, password});
-  logOut();
-  logInTests({username, password});
-}
+    registerUser({username, password});
+    logOut();
+    logInTests({username, password});
+}    
