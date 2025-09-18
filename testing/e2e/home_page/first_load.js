434d6d6da117e686180af4a147380cac0ccc5559
root <root@DESKTOP-F4B1KO7.localdomain>
2025-09-17 18:14:06 -0700

    do load video stuff

--- testing/e2e/home_page/first_load.js
+++ testing/e2e/home_page/first_load.js
@@ -9,20 +9,22 @@ class FirstLoad {
 
   loadVideos() {
     // Wait up to 10 seconds for the video list to appear
-    cy.get('[data-testid=video-list]', { timeout: 10000 }).should('exist');
+    cy.get('[data-testid=home-videos-list]', { timeout: 10000 }).should('exist');
 
     // Make sure at least one video thumbnail is loaded
-    cy.get('[data-testid=video-item]').should('have.length.greaterThan', 0);
+    cy.get('[data-testid=home-video-list-item]').should('have.length.greaterThan', 0);
   };
 
   navigateToWatchVideo(video_name) {
     // Click on the first video
-    cy.get('[data-testid=video-item]').first().click();
+    cy.get('[data-testid=home-video-list-item]').first().click();
 
     // Check that the video player appears
     cy.get('[data-testid=video-player]', { timeout: 10000 }).should('be.visible');
 
-    // Optional: check that video metadata (title, description) is visible
-    cy.get('[data-testid=video-title]').should('not.be.empty');
+    cy.get('video[data-testid="video-player"]').then($video => {
+    const video = $video[0];
+        expect(video.duration).to.be.greaterThan(0); // TODO: get precise duration of video?
+    });
   };
 }
