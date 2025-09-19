050588a90fb202999d90ded7f087135d8a99eae2
quichey <kylengo357@gmail.com>
2025-09-19 16:45:00 -0700

    add code to selenium_profile.py

---
+++ testing_2/profiles/selenium_profile.py
@@ -0,0 +1,8 @@
+# profiles/selenium_profile.py
+
+# Path to the Chrome profile you created manually
+SELENIUM_PROFILE_PATH = "profiles/selenium_profile_folder"
+
+# Optional: you can add other profile-specific options here
+HEADLESS = True  # whether to run tests headless
+WINDOW_SIZE = "1920,1080"
