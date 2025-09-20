677e02d5837cd6a8bf94317ca4696a82fcfe6aa7
quichey <kylengo357@gmail.com>
2025-09-19 18:59:10 -0700

    fill in the func

--- testing_2/profiles/selenium_profile.py
+++ testing_2/profiles/selenium_profile.py
@@ -1,4 +1,5 @@
-# profiles/selenium_profile.py
+import shutil
+import os
 
 # Path to the Chrome profile you created manually
 SELENIUM_PROFILE_PATH = "profiles/selenium_profile_folder"
@@ -9,4 +10,17 @@ WINDOW_SIZE = "1920,1080"
 
 
 def borrow_creds():
-    pass
+    SOURCE_PROFILE = r"C:\Users\<YourUser>\AppData\Local\Google\Chrome\User Data\SeleniumGoldenProfile"
+    DEST_PROFILE = r"./profiles/selenium_profile_folder"
+    # Remove old test profile
+    if os.path.exists(DEST_PROFILE):
+        shutil.rmtree(DEST_PROFILE)
+
+    # Copy essential files/folders
+    shutil.copytree(
+        SOURCE_PROFILE,
+        DEST_PROFILE,
+        ignore=shutil.ignore_patterns(
+            "LOCK", "Sessions", "Sync Data", "GPUCache", "Cache"
+        ),
+    )
