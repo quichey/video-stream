6fef8268d0e056f99316a0286d84ff8c93e3af46
root <root@DESKTOP-F4B1KO7.localdomain>
2025-09-19 18:56:09 -0700

    create func to handle google login refresh

--- testing_2/profiles/selenium_profile.py
+++ testing_2/profiles/selenium_profile.py
@@ -6,3 +6,7 @@ SELENIUM_PROFILE_PATH = "profiles/selenium_profile_folder"
 # Optional: you can add other profile-specific options here
 HEADLESS = True  # whether to run tests headless
 WINDOW_SIZE = "1920,1080"
+
+
+def borrow_creds():
+    pass
