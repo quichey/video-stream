15ba1c824615818d8efc101a4bc28b6c0cb7e08d
root <root@DESKTOP-F4B1KO7.localdomain>
2025-09-20 16:56:07 -0700

    make parse_common_args

--- cloud/deploy/deploy_machines.py
+++ cloud/deploy/deploy_machines.py
@@ -1,49 +1,30 @@
-import argparse
-
 from client.client_deployer import ClientDeployer
 from server.server_deployer import ServerDeployer
+from util.script_args import parse_common_args
 
-"""
-If no args given, deploy all machines
-"""
 
 def deploy_instances(machines):
-    for Deployer in machines:
-        Deployer.deploy()
+    for deployer in machines:
+        deployer.deploy()
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-
-    # cloud provider option
-    parser.add_argument("--cloud_provider", default="Azure", help="Cloud Provider")
-    parser.add_argument(
-        "--env",
-        default="dev",
-        choices=["prod", "stage", "dev", "test"],
-        help="Deployment environment (prod/stage/dev/test)"
-    )
-
-    parser.add_argument("--client", action="store_true", help="Deploy client service")
-    parser.add_argument("--server", action="store_true", help="Deploy server service")
-
-    args = parser.parse_args()
+    args = parse_common_args()
 
     print(f"Cloud-Provider: {args.cloud_provider}")
     print(f"Deployment Environment: {args.env}")
-
     print(f"Client: {args.client}")
     print(f"Server: {args.server}")
-    if args.client is None and args.server is None:
-        print("No Machines specified -- Deploying All")
 
     client_deployer = ClientDeployer(provider_name=args.cloud_provider, env=args.env)
     server_deployer = ServerDeployer(provider_name=args.cloud_provider, env=args.env)
 
-    default_all = [client_deployer, server_deployer]
+    # If no machines specified, deploy all
     machines = []
     if args.client:
         machines.append(client_deployer)
     if args.server:
         machines.append(server_deployer)
-    machines = default_all if len(machines) == 0 else machines
+    machines = machines or [client_deployer, server_deployer]
+
     deploy_instances(machines)
--- testing_2/drivers/devices.py
+++ testing_2/drivers/devices.py
@@ -1,1 +1,8 @@
 # TODO: handle cloud services/devices
+
+# extract env vars from cloud/providers/azure etc.
+
+# call cloud/deploy/deploy_machines --env test in here?
+
+# should the python module to start/stop cloud services be in here?
+# prob not, prob better in cloud/ somewhere, probably deploy?
