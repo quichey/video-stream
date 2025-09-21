7cc9c9c2d985ea626f2f33c085ab99e08e051ce8
root <root@DESKTOP-F4B1KO7.localdomain>
2025-09-21 16:50:32 -0700

    fix code

--- testing_2/run_tests.py
+++ testing_2/run_tests.py
@@ -1,9 +1,8 @@
 from drivers.devices import TestInfraManager
-from drivers.driver import create_driver
+from drivers.driver import driver
 
 
 if __name__ == "__main__":
-    driver = create_driver()
     infra_manager = TestInfraManager()
 
     infra_manager.deploy()
