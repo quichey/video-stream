d8b740b8b02ae023e1ce87ee36694a4d17296a27
quichey <kylengo357@gmail.com>
2025-09-20 20:08:39 -0700

    write the run_cmds

--- testing_2/drivers/devices.py
+++ testing_2/drivers/devices.py
@@ -7,6 +7,18 @@
 # should the python module to start/stop cloud services be in here?
 # prob not, prob better in cloud/ somewhere, probably deploy?
 
+import subprocess
+
+
+def run_cmds(cmd_array, **kwargs):
+    if type(cmd_array[0]) == str:
+        return subprocess.run(cmd_array, **kwargs)
+    else:
+        for cmd in cmd_array:
+            # TODO: what do return for this case?
+            subprocess.run(cmd, **kwargs)
+
+
 SERVICE_LIFECYCLE_SCRIPT_DIR = "cloud/deploy"
 SERVICE_LIFECYCLE_SCRIPT_NAME = "service_lifecycle.py"
 SERVICE_LIFECYCLE_SCRIPT_LOCATION = (
@@ -22,6 +34,14 @@ class TestInfraManager:
         self._run_lifecycle_script("stop")
 
     def _run_lifecycle_script(self, action):
-        cd_cmd = f"cd {SERVICE_LIFECYCLE_SCRIPT_DIR}"
-
-        run_cmd = f"poetry run python3 {SERVICE_LIFECYCLE_SCRIPT_NAME} --env test --action {action}"
+        run_cmd = [
+            "poetry",
+            "run",
+            "python3",
+            SERVICE_LIFECYCLE_SCRIPT_NAME,
+            "--env",
+            "test",
+            "--action",
+            action,
+        ]
+        run_cmds(run_cmd, cwd=f"../../{SERVICE_LIFECYCLE_SCRIPT_DIR}")
