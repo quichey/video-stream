fc4035b6af8ae993ae7273d19973d127ba713022
root <root@DESKTOP-F4B1KO7.localdomain>
2025-09-21 19:35:20 -0700

    call borrow_creds

--- testing_2/drivers/driver.py
+++ testing_2/drivers/driver.py
@@ -1,9 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from profiles.selenium_profile import SELENIUM_PROFILE_PATH, HEADLESS, WINDOW_SIZE
+from profiles.selenium_profile import (
+    SELENIUM_PROFILE_PATH,
+    HEADLESS,
+    WINDOW_SIZE,
+    borrow_creds,
+)
 
 
 def create_driver():
+    borrow_creds()
     chrome_options = Options()
     if SELENIUM_PROFILE_PATH:
         chrome_options.add_argument(f"--user-data-dir={SELENIUM_PROFILE_PATH}")
