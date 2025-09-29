5c7d5d56133aef62cd75856e18c3d1e2ccb8126b
root <root@DESKTOP-F4B1KO7.localdomain>
2025-09-29 10:33:41 -0700

    get rid of pre_build_hook from cloudmixin

--- cloud/deploy/common/mixins/cloud_mixin/cloud_mixin.py
+++ cloud/deploy/common/mixins/cloud_mixin/cloud_mixin.py
@@ -1,17 +1,3 @@
-def pre_build_hook(func):
-    """Decorator to run a pre-build step if the subclass/provider defines it."""
-
-    def wrapper(self, *args, **kwargs):
-        # Call pre-build step if provider has it
-        pre_build = getattr(self.provider, "pre_build_image_cloud", None)
-        if callable(pre_build):
-            print(f"[CloudMixin] Running pre-build step for {self.context}...")
-            pre_build(*args, **kwargs)
-        return func(self, *args, **kwargs)
-
-    return wrapper
-
-
 class CloudMixin:
     GET_PROVIDER_CLASS_FUNC = None
 
