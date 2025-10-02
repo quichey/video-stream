4c676751ae07a87396462e166b0f9eb4e1c1bb7a
quichey <kylengo357@gmail.com>
2025-10-02 16:25:15 -0700

    add file for purely datasets

---
+++ server/db/Seed/datasets.py
@@ -0,0 +1,25 @@
+testing_state_small = {
+    "tables_random_populate": [
+        {
+            "name": "users",
+            "num_records": 2,  # 10
+        },
+        {"name": "videos", "num_records": 2},
+        {
+            "name": "comments",
+            "num_records": 10000,
+        },
+        {
+            "name": "user_cookies",
+            "num_records": 0,
+        },
+        {
+            "name": "third_party_auth_users",
+            "num_records": 0,
+        },
+        {
+            "name": "third_party_auth_tokens",
+            "num_records": 0,
+        },
+    ]
+}
