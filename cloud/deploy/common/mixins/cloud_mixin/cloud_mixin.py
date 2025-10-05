104ec24b0152ddde1c7c3f69dce5a9fd1b844236
quichey <kylengo357@gmail.com>
2025-10-04 21:29:05 -0700

    pass in dialect arg to instance creation

--- cloud/deploy/common/mixins/cloud_mixin/cloud_mixin.py
+++ cloud/deploy/common/mixins/cloud_mixin/cloud_mixin.py
@@ -1,9 +1,11 @@
 class CloudMixin:
     GET_PROVIDER_CLASS_FUNC = None
 
-    def __init__(self, provider_name, context, env):
+    def __init__(self, provider_name, context, env, *args, **kwargs):
         self.context = context
-        self._provider = self.GET_PROVIDER_CLASS_FUNC(provider_name)(context, env)
+        self._provider = self.GET_PROVIDER_CLASS_FUNC(provider_name, *args, **kwargs)(
+            context, env
+        )
 
     def set_up_provider_env(self):
         self.provider.set_up_env()
