bd17b72981e2bb84ad5e9b2d8093516147cc9bc3
root <root@DESKTOP-F4B1KO7.localdomain>
2025-10-20 18:34:41 -0700

    fill in DB_Connection

--- server/db/migrations/db_connection.py
+++ server/db/migrations/db_connection.py
@@ -5,7 +5,8 @@ import sys
 # Alembic imports
 
 # SQLAlchemy imports
-
+from db.Schema import database_specs, database_specs_cloud_sql
+from db.util.connections import construct_conn_str_any
 
 # --- CRITICAL PATH FIX ---
 # Add the project's root directory to the system path.
@@ -17,6 +18,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".
 class DB_Connection:
     def __init__(self):
         self.DEPLOYMENT = os.environ.get("DEPLOYMENT")
+        if self.DEPLOYMENT == "local":
+            self.DB_SPECS = database_specs
+        else:
+            self.DB_SPECS = database_specs_cloud_sql
 
     def get_connection_str(self):
-        pass
+        return construct_conn_str_any(self.DB_SPECS, self.DEPLOYMENT)
